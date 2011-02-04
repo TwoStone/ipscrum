cd044dd77d481ca036b0a1f84e94eb0eac72ba67
ha\hfw408wo <ha\hfw408wo@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-04 20:31:02 +0000

    Schnittstellen für Gruppe 3 Vorbereitet. Diese werden von Gruppe 1 benötigt, da auf diesen implementiert wird.

---
+++ IpScrum/src/fhdw/ipscrum/shared/model/interfaces/IPerson.java
@@ -0,0 +1,6 @@
+package fhdw.ipscrum.shared.model.interfaces;
+
+public interface IPerson {
+
+	public Boolean deleatable();
+}
---
+++ IpScrum/src/fhdw/ipscrum/shared/model/interfaces/ISprint.java
@@ -0,0 +1,7 @@
+package fhdw.ipscrum.shared.model.interfaces;
+
+public interface ISprint {
+
+	public Boolean deleatable();
+	
+}
