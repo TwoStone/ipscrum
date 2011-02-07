8db7b06459103965a5b949b3b6bb5b749fea7ed7
ha\hfw408wo <ha\hfw408wo@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-07 16:37:10 +0000

    Angepasst dahingehend, dass zunächst nur noch der RootPresenter erzeugt wird und ihm das mainPanel übergeben wird.

--- IpScrum/src/fhdw/ipscrum/client/IpScrum.java
+++ IpScrum/src/fhdw/ipscrum/client/IpScrum.java
@@ -1,15 +1,9 @@
 package fhdw.ipscrum.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
-import fhdw.ipscrum.client.view.ProductBacklogView;
-import fhdw.ipscrum.client.view.ProjectCreateView;
-import fhdw.ipscrum.client.view.ProjectOverviewView;
-import fhdw.ipscrum.client.view.StartLoggedInView;
-import fhdw.ipscrum.client.view.StartView;
+import fhdw.ipscrum.client.presenter.RootPresenter;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -17,29 +11,10 @@ import fhdw.ipscrum.client.view.StartView;
 public class IpScrum implements EntryPoint {
 	
 	/**
-	 * Panel containing the GWT stuff
-	 * Change to whatever panel implementation is useful!
-	 */
-	private static Panel mainPanel =  new VerticalPanel();
-	
-	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-
-		
-		RootPanel rootPanel = RootPanel.get("mainPanel");
-		
-ProductBacklogView pb = new ProductBacklogView();
-StartLoggedInView einstieg = new StartLoggedInView();
-StartView einstieg_angemeldet = new StartView();
-ProjectCreateView pe = new ProjectCreateView();
-ProjectOverviewView pue = new ProjectOverviewView();
-//DialogBox dialog = new DialogBox();
-//dialog.add(einstieg);
-//dialog.show();
-
-rootPanel.add(pb); 	
+		new RootPresenter(RootPanel.get("mainPanel"));
 	}
  }
 	
