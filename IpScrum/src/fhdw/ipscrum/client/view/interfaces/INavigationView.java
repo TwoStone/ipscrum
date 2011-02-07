5daedbae51acdecef3b44c11954e431066363825
ha\hfw408he <ha\hfw408he@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-07 19:11:41 +0000

    NavigationView und Interface

---
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/INavigationView.java
@@ -0,0 +1,15 @@
+package fhdw.ipscrum.client.view.interfaces;
+
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.FlowPanel;
+
+public interface INavigationView {
+
+	public abstract HasClickHandlers getPersonalverwaltung();
+	
+	public abstract HasClickHandlers getProjekte();
+	
+	public abstract FlowPanel getMasterMainPanel();
+	
+	
+}
