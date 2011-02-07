3f99bf47590c09dfbc8dd11520e4ee347a03da1a
ha\hfw408he <ha\hfw408he@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-07 19:12:35 +0000

    INavigation erweiter IView

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/INavigationView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/INavigationView.java
@@ -3,7 +3,7 @@ package fhdw.ipscrum.client.view.interfaces;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.user.client.ui.FlowPanel;
 
-public interface INavigationView {
+public interface INavigationView extends IView{
 
 	public abstract HasClickHandlers getPersonalverwaltung();
 	
