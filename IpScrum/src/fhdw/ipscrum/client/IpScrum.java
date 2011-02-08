b924b9f320da11820ea261547cae06460e3436f8
ha\hfw408wo <ha\hfw408wo@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-08 18:33:45 +0000

    DummyDaten ergänzt für ProductBackloItems

--- IpScrum/src/fhdw/ipscrum/client/IpScrum.java
+++ IpScrum/src/fhdw/ipscrum/client/IpScrum.java
@@ -5,6 +5,8 @@ import com.google.gwt.user.client.ui.RootPanel;
 
 import fhdw.ipscrum.client.presenter.RootPresenter;
 import fhdw.ipscrum.shared.SessionManager;
+import fhdw.ipscrum.shared.model.ProductBacklog;
+import fhdw.ipscrum.shared.model.ProductBacklogItem;
 import fhdw.ipscrum.shared.model.Project;
 
 /**
@@ -16,7 +18,15 @@ public class IpScrum implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		SessionManager.getInstance().getModel().addProject(new Project("Testprojekt 1"));
+		Project p1 = new Project("Testprojekt 1");
+		ProductBacklog bl = new ProductBacklog();
+		bl.addItem(new ProductBacklogItem("Test",2,bl,null) {
+		});
+		bl.addItem(new ProductBacklogItem("Test 2",5,bl,null) {
+		});
+		p1.setBacklog(bl);
+		
+		SessionManager.getInstance().getModel().addProject(p1);
 		SessionManager.getInstance().getModel().addProject(new Project("Testprojekt 2"));
 		SessionManager.getInstance().getModel().addProject(new Project("Testprojekt 3"));
 		SessionManager.getInstance().getModel().addProject(new Project("Testprojekt 4"));
