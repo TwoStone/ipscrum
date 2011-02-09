2002da8c3fb338f48c6064e2708a966fb2ffbbde
ha\hfw408wo <ha\hfw408wo@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-09 19:44:52 +0000

    Neue Anforderungen gemäß A. Brack! Grund: -PBI wird Sprint und Sprint wird Release zugeordnet -Sprint Reverse Assotiation

--- IpScrum/src/fhdw/ipscrum/shared/model/interfaces/ISprint.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/interfaces/ISprint.java
@@ -1,7 +1,9 @@
 package fhdw.ipscrum.shared.model.interfaces;
 
 import java.sql.Date;
+import java.util.Vector;
 
+import fhdw.ipscrum.shared.model.ProductBacklogItem;
 import fhdw.ipscrum.shared.model.Team;
 
 public interface ISprint {
@@ -14,4 +16,14 @@ public interface ISprint {
 	
 	public Team getTeam();
 	public void setTeam(Team team);
+	
+	//TODO PWOLF Modelländerung gemäß Brack, falls unklar mich bitte ansprechen
+	//Achtung! Bitte konsistenzbedingung einarbeiten, dass keine neuen
+	//Releases definiert werden dürfen, sondern nur Releases hinzugefügt werden,
+	//die bereits einem Project anhängen!
+	public void setRelease(IRelease release);
+	public IRelease getRelease();
+
+	//TODO Reverse Assoziation!
+	public Vector<ProductBacklogItem> getPBIs();
 }
