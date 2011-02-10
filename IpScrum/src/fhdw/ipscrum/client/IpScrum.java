3a047927314551f1c95118c0ceff5b8d683bbd75
ha\hfw408hu <ha\hfw408hu@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-10 21:40:27 +0000

    Team-Demodaten

--- IpScrum/src/fhdw/ipscrum/client/IpScrum.java
+++ IpScrum/src/fhdw/ipscrum/client/IpScrum.java
@@ -5,10 +5,10 @@ import com.google.gwt.user.client.ui.RootPanel;
 
 import fhdw.ipscrum.client.presenter.RootPresenter;
 import fhdw.ipscrum.shared.SessionManager;
-import fhdw.ipscrum.shared.exceptions.NoValidValueException;
 import fhdw.ipscrum.shared.model.Person;
 import fhdw.ipscrum.shared.model.Project;
 import fhdw.ipscrum.shared.model.Role;
+import fhdw.ipscrum.shared.model.Team;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -69,13 +69,25 @@ public class IpScrum implements EntryPoint {
 		SessionManager.getInstance().getModel().addPerson(pChristin);
 		SessionManager.getInstance().getModel().addPerson(pNils);
 		
+		// Initial Teams
+		Team t1 = new Team("Frontend");
+		t1.addMember(pSarah);
+		t1.addMember(pWilken);
+
+		Team t2 = new Team ("Backend");
+		t2.addMember(pChristin);
+		t2.addMember(pNils);
+		
+		SessionManager.getInstance().getModel().addTeam(t1);
+		SessionManager.getInstance().getModel().addTeam(t2);
+		
 		// Initial Projects
 		try {
 			SessionManager.getInstance().getModel().addProject(new Project("Testprojekt 1"));
 			SessionManager.getInstance().getModel().addProject(new Project("Testprojekt 2"));
 			SessionManager.getInstance().getModel().addProject(new Project("Testprojekt 3"));
 		} catch (NoValidValueException e) {
-			//Da sp‰ter entfernt wird, wird diese Meldung zun‰chst nicht beachtet!
+			//Da spä‰ter entfernt wird, wird diese Meldung zun‰chst nicht beachtet!
 		}
 	}
  }
