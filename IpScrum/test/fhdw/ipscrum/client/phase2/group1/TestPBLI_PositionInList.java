eda5a64d50f7b7f2412c35b85c3df558011e78f2
ha\hfw408ho <ha\hfw408ho@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-12 13:31:27 +0000

    Testfälle für moveUp und moveDown, Testfälle 12, 14, 17, 19

---
+++ IpScrum/test/fhdw/ipscrum/client/phase2/group1/TestPBLI_PositionInList.java
@@ -0,0 +1,79 @@
+package fhdw.ipscrum.client.phase2.group1;
+
+import org.apache.tools.ant.types.CommandlineJava.SysProperties;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+import static org.junit.Assert.*;
+import fhdw.ipscrum.shared.model.Feature;
+import fhdw.ipscrum.shared.model.ProductBacklog;
+import fhdw.ipscrum.shared.model.ProductBacklogItem;
+import fhdw.ipscrum.shared.model.Project;
+
+public class TestPBLI_PositionInList {
+
+	private static Project test = null;
+	private static ProductBacklog pbltest = null;
+	private static Feature a = null;
+	private static Feature b = null;
+	private static Feature c = null;
+	private static Feature d = null;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		test = new Project("test");
+		pbltest = test.getBacklog();
+		pbltest.getItems();
+
+		a = new Feature("A", "testA", pbltest);
+		b = new Feature("B", "testB", pbltest);
+		c = new Feature("C", "testC", pbltest);
+		d = new Feature("D", "testD", pbltest);
+
+		// System.out.println(pbltest.getItems().toString());
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testMoveUp1() throws Exception {
+		ProductBacklogItem item = (ProductBacklogItem) pbltest.getItems().get(1);
+		int positionOfItemNo2 = pbltest.getItemPositionInList(item);
+		pbltest.moveUp(item);
+		assertEquals(b, pbltest.getItems().get(positionOfItemNo2 - 1));
+	}
+
+	@Test
+	public void testMoveUp2() throws Exception {
+		ProductBacklogItem holder = pbltest.getItems().get(0);
+		pbltest.moveUp(holder);
+		assertEquals(holder, pbltest.getItems().get(0));
+	}
+	
+	@Test
+	public void testMoveDown1() throws Exception {
+		ProductBacklogItem item = (ProductBacklogItem) pbltest.getItems().get(0);
+		int positionOfItemNo1 = pbltest.getItemPositionInList(item);
+		pbltest.moveDown(item);
+		assertEquals(b, pbltest.getItems().get(positionOfItemNo1 + 1));
+	}
+
+	@Test
+	public void testMoveDown2() throws Exception {
+		int lastPositionInList = pbltest.getItems().size() - 1;
+		ProductBacklogItem holder = pbltest.getItems().get(lastPositionInList);
+		pbltest.moveDown(holder);
+		assertEquals(holder, pbltest.getItems().get(lastPositionInList));
+	}
+	
+	
+	
+	
+}
