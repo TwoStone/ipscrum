dfb2699791f62954e3f1848e689c5126aae5a9d3
ha\hfw408wa <ha\hfw408wa@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-12 10:25:23 +0000

    Root implementiert nun HasRelationTypeManager RelationType hat eine FactoryMethode, die das globale Speichern übernimmt.

--- IpScrum/src/fhdw/ipscrum/shared/model/RelationType.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/RelationType.java
@@ -1,41 +1,61 @@
 package fhdw.ipscrum.shared.model;
+
+import fhdw.ipscrum.shared.SessionManager;
+import fhdw.ipscrum.shared.exceptions.DoubleDefinitionException;
+
 /**
- * A relation type represents a customer-specified type of a relation
- * between features (see {@link Feature}.
+ * A relation type represents a customer-specified type of a relation between
+ * features (see {@link Feature}.
  */
 public class RelationType {
-	private String description;
+	public static RelationType create(final String description)
+			throws DoubleDefinitionException {
+		final RelationType type = new RelationType(description);
+		SessionManager.getInstance().getModel().getRelationTypeManager()
+				.addRelationType(type);
+		return type;
+	}
 
-	public RelationType(String description){
+	private final String description;
+
+	private RelationType(final String description) {
 		this.description = description;
 	}
+
 	public String getDescription() {
-		return description;
+		return this.description;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((description == null) ? 0 : description.hashCode());
+		result = prime
+				* result
+				+ ((this.description == null) ? 0 : this.description.hashCode());
 		return result;
 	}
+
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
+	public boolean equals(final Object obj) {
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (this.getClass() != obj.getClass()) {
 			return false;
-		RelationType other = (RelationType) obj;
-		if (description == null) {
-			if (other.description != null)
+		}
+		final RelationType other = (RelationType) obj;
+		if (this.description == null) {
+			if (other.description != null) {
 				return false;
-		} else if (!description.equals(other.description))
+			}
+		} else if (!this.description.equals(other.description)) {
 			return false;
+		}
 		return true;
 	}
-	
-	
+
 }
--- IpScrum/src/fhdw/ipscrum/shared/model/Root.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/Root.java
@@ -3,6 +3,7 @@ package fhdw.ipscrum.shared.model;
 import java.util.HashSet;
 
 import fhdw.ipscrum.shared.exceptions.PersistenceException;
+import fhdw.ipscrum.shared.model.interfaces.HasRelationTypeManager;
 import fhdw.ipscrum.shared.model.interfaces.IPerson;
 import fhdw.ipscrum.shared.model.interfaces.IRole;
 import fhdw.ipscrum.shared.model.interfaces.ITeam;
@@ -10,131 +11,138 @@ import fhdw.ipscrum.shared.observer.Observable;
 import fhdw.ipscrum.shared.persistence.SerializationRoot;
 
 /**
- * This class represent the root point for the whole model.
- * This class is additionally used for storing project independent
- * data like teams, persons and roles.
+ * This class represent the root point for the whole model. This class is
+ * additionally used for storing project independent data like teams, persons
+ * and roles.
  */
-public class Root extends Observable implements SerializationRoot{
+public class Root extends Observable implements SerializationRoot,
+		HasRelationTypeManager {
 
 	private HashSet<Project> projects;
 	private HashSet<IPerson> persons;
 	private HashSet<ITeam> teams;
 	private HashSet<IRole> roles;
-	
+	private RelationTypeManager relationTypeManager;
+
 	@Override
-	public void save(String identifier) throws PersistenceException {
+	public void save(final String identifier) throws PersistenceException {
 		// No Implementation Needed
 	}
-	
+
 	public HashSet<IPerson> getPersons() {
-		if(this.persons==null){
+		if (this.persons == null) {
 			this.persons = new HashSet<IPerson>();
 		}
-		return persons;
+		return this.persons;
 	}
-	
+
 	public HashSet<Project> getProjects() {
-		if(this.projects==null){
+		if (this.projects == null) {
 			this.projects = new HashSet<Project>();
 		}
-		return projects;
+		return this.projects;
 	}
-	
+
 	public HashSet<ITeam> getTeams() {
-		if(this.teams==null){
+		if (this.teams == null) {
 			this.teams = new HashSet<ITeam>();
 		}
-		return teams;
+		return this.teams;
 	}
-	
+
 	public HashSet<IRole> getRoles() {
-		if(this.roles==null){
+		if (this.roles == null) {
 			this.roles = new HashSet<IRole>();
 		}
-		return roles;
+		return this.roles;
 	}
-	
+
+	@Override
+	public RelationTypeManager getRelationTypeManager() {
+		if (this.relationTypeManager == null) {
+			this.relationTypeManager = new RelationTypeManager();
+		}
+		return this.relationTypeManager;
+	}
+
 	/**
-	 * Removes the given project with all depending data from
-	 * the model.
+	 * Removes the given project with all depending data from the model.
+	 * 
 	 * @param project
-	 * Project for Deletion!
+	 *            Project for Deletion!
 	 */
-	public void removeProject(Project project){
+	public void removeProject(final Project project) {
 		this.getProjects().remove(project);
 		this.notifyObservers();
 	}
-	
+
 	/**
 	 * Add a new Project to the model.
+	 * 
 	 * @param project
 	 */
-	public void addProject(Project project){
+	public void addProject(final Project project) {
 		this.getProjects().add(project);
 		this.notifyObservers();
 	}
-	
+
 	/**
-	 * Returns the number of Projects within the
-	 * model!
+	 * Returns the number of Projects within the model!
 	 */
-	public Integer countProjects(){
+	public Integer countProjects() {
 		return this.getProjects().size();
 	}
-	
+
 	/**
 	 * Adds a new person to the model!
 	 */
-	public void addPerson(IPerson person){
+	public void addPerson(final IPerson person) {
 		this.getPersons().add(person);
 		this.notifyObservers();
 	}
-	
+
 	/**
-	 * Returns the number of all persons
-	 * within the Model.
+	 * Returns the number of all persons within the Model.
 	 */
-	public Integer countPersons(){
+	public Integer countPersons() {
 		return this.getPersons().size();
 	}
-	
+
 	/**
 	 * Adds a new Team to the model.
 	 */
-	public void addTeam(ITeam team){
+	public void addTeam(final ITeam team) {
 		this.getTeams().add(team);
 		this.notifyObservers();
 	}
-	
+
 	/**
-	 * Returns the number of all defined
-	 * teams.
+	 * Returns the number of all defined teams.
 	 */
-	public Integer countTeams(){
+	public Integer countTeams() {
 		return this.getTeams().size();
 	}
 
 	/**
-	 * Removes the given role.
-	 * TODO Check Consistency
+	 * Removes the given role. TODO Check Consistency
 	 */
-	public void removeRole(IRole role){
+	public void removeRole(final IRole role) {
 		this.getRoles().remove(role);
 		this.notifyObservers();
 	}
-	
+
 	/**
 	 * Adds a new Role to the model.
 	 */
-	public void addRole(IRole role){
+	public void addRole(final IRole role) {
 		this.getRoles().add(role);
 		this.notifyObservers();
 	}
-	
+
 	/**
 	 * Returns the number of all Roles.
 	 */
-	public Integer countRoles(){
+	public Integer countRoles() {
 		return this.getRoles().size();
 	}
 
@@ -147,43 +155,61 @@ public class Root extends Observable implements SerializationRoot{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((this.getPersons() == null) ? 0 : this.getPersons().hashCode());
+		result = prime
+				* result
+				+ ((this.getPersons() == null) ? 0 : this.getPersons()
+						.hashCode());
+		result = prime
+				* result
+				+ ((this.getProjects() == null) ? 0 : this.getProjects()
+						.hashCode());
 		result = prime * result
-				+ ((this.getProjects() == null) ? 0 : this.getProjects().hashCode());
-		result = prime * result + ((this.getRoles() == null) ? 0 : this.getRoles().hashCode());
-		result = prime * result + ((this.getTeams() == null) ? 0 : this.getTeams().hashCode());
+				+ ((this.getRoles() == null) ? 0 : this.getRoles().hashCode());
+		result = prime * result
+				+ ((this.getTeams() == null) ? 0 : this.getTeams().hashCode());
 		return result;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
+	public boolean equals(final Object obj) {
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (this.getClass() != obj.getClass()) {
 			return false;
-		Root other = (Root) obj;
+		}
+		final Root other = (Root) obj;
 		if (this.getPersons() == null) {
-			if (other.getPersons() != null)
+			if (other.getPersons() != null) {
 				return false;
-		} else if (!this.getPersons().equals(other.getPersons()))
+			}
+		} else if (!this.getPersons().equals(other.getPersons())) {
 			return false;
+		}
 		if (this.getProjects() == null) {
-			if (other.getProjects() != null)
+			if (other.getProjects() != null) {
 				return false;
-		} else if (!this.getProjects().equals(other.getProjects()))
+			}
+		} else if (!this.getProjects().equals(other.getProjects())) {
 			return false;
+		}
 		if (this.getRoles() == null) {
-			if (other.getRoles() != null)
+			if (other.getRoles() != null) {
 				return false;
-		} else if (!this.getRoles().equals(other.getRoles()))
+			}
+		} else if (!this.getRoles().equals(other.getRoles())) {
 			return false;
+		}
 		if (this.getTeams() == null) {
-			if (other.getTeams() != null)
+			if (other.getTeams() != null) {
 				return false;
-		} else if (!this.getTeams().equals(other.getTeams()))
+			}
+		} else if (!this.getTeams().equals(other.getTeams())) {
 			return false;
+		}
 		return true;
 	}
 }
