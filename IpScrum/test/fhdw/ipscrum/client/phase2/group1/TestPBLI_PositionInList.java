b0f9639ea51e403e8a8b5e31af25f28c8a3e5b05
ha\hfw408ho <ha\hfw408ho@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-12 18:34:04 +0000

    Test auf doppeltes Release eingefügt Test der Equals

--- IpScrum/test/fhdw/ipscrum/client/phase2/group1/TestPBLI_PositionInList.java
+++ IpScrum/test/fhdw/ipscrum/client/phase2/group1/TestPBLI_PositionInList.java
@@ -1,14 +1,23 @@
 package fhdw.ipscrum.client.phase2.group1;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+
 import org.junit.Test;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 import static org.junit.Assert.*;
+import fhdw.ipscrum.shared.exceptions.DoubleDefinitionException;
 import fhdw.ipscrum.shared.model.Feature;
 import fhdw.ipscrum.shared.model.ProductBacklog;
 import fhdw.ipscrum.shared.model.ProductBacklogItem;
 import fhdw.ipscrum.shared.model.Project;
+import fhdw.ipscrum.shared.model.Release;
+import fhdw.ipscrum.shared.model.Root;
+import fhdw.ipscrum.shared.model.Sprint;
+import fhdw.ipscrum.shared.model.Team;
 
 public class TestPBLI_PositionInList {
 
@@ -18,11 +27,18 @@ public class TestPBLI_PositionInList {
 	private static Feature b = null;
 	private static Feature c = null;
 	private static Feature d = null;
+	private static Date cvCurrentDate = null;
+	private static Date cvCurrentDate2 = null;
 
+	@SuppressWarnings("deprecation")
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		test = new Project("test");
 		pbltest = test.getBacklog();
+		
+		cvCurrentDate = new Date();
+		cvCurrentDate2 = cvCurrentDate;
+		cvCurrentDate2.setMonth(Calendar.MONTH+1);
 
 		a = new Feature("A", "testA", pbltest);
 		b = new Feature("B", "testB", pbltest);
@@ -44,6 +60,11 @@ public class TestPBLI_PositionInList {
 		assertEquals(item, pbltest.getItems().get(positionOfItemNo2 - 1));
 	}
 
+	
+	/**
+	 * Move Up auf erstes Element der Liste
+	 * @throws Exception
+	 */
 	@Test
 	public void testMoveUp2() throws Exception {
 		ProductBacklogItem holder = pbltest.getItems().get(0);
@@ -59,6 +80,10 @@ public class TestPBLI_PositionInList {
 		assertEquals(item, pbltest.getItems().get(positionOfItemNo1 + 1));
 	}
 
+	/**
+	 * Move Down auf letztes Element der Liste
+	 * @throws Exception
+	 */
 	@Test
 	public void testMoveDown2() throws Exception {
 		int lastPositionInList = pbltest.getItems().size() - 1;
@@ -67,7 +92,71 @@ public class TestPBLI_PositionInList {
 		assertEquals(holder, pbltest.getItems().get(lastPositionInList));
 	}
 	
+	@Test
+	public void testAddPBI() throws Exception {
+		int entriesInList = pbltest.getItems().size();
+		new Feature("E", "Test E", pbltest);
+		// System.out.println(pbltest.getItems().get(entriesInList));
+		assertEquals(entriesInList + 1 , pbltest.getItems().size());
+	}
 	
+	@Test
+	public void testDeletePBI() throws Exception {
+		int entriesInList = pbltest.getItems().size();
+		pbltest.removeItem(pbltest.getItems().lastElement());
+		
+		assertEquals(entriesInList - 1 , pbltest.getItems().size());
+	}
 	
+	@Test
+	public void testCount1() throws Exception {
+		Project lvProject = new Project("wayne");
+		assertEquals(0, lvProject.getBacklog().getItems().size());
+	}
+	
+	@Test
+	public void testCount2() throws Exception {
+		Project lvProject = new Project("wayne");
+		assertEquals(0, lvProject.getBacklog().getItems().size());
+		new Feature("1", "", lvProject.getBacklog());
+		new Feature("2", "", lvProject.getBacklog());
+		new Feature("3", "", lvProject.getBacklog());
+		new Feature("4", "", lvProject.getBacklog());
+	
+		assertEquals(4 , lvProject.getBacklog().getItems().size());
+	}
+	
+	@Test
+	public void testAddRelease() throws Exception {
+		int holder = test.getReleasePlan().size();
+		new Release("0.0.1", cvCurrentDate, test);
+		
+		assertEquals(holder + 1, test.getReleasePlan().size());
+	}
+	
+	@Test
+	public void testAddRelease2(){
+		int holder = test.getReleasePlan().size();
+		try {
+			new Release("0.0.1", cvCurrentDate, test);
+			
+			fail("keine Fehlermeldung gekommen");
+		} catch (DoubleDefinitionException e) {
+			assertEquals(holder, test.getReleasePlan().size());
+		}
+	}
+	
+	@Test
+	public void testProjectEquals() throws Exception {
+		Project lvProject = test;
+		assertEquals(lvProject, test);
+	}
+	
+	@Test
+	public void testPBLEquals() throws Exception {
+		ProductBacklog lvBacklog = pbltest;
+		assertEquals(lvBacklog, pbltest);
+	}
+
 	
 }
