0507ed71aa5ddbd65f3f0fc10b62766e4ce104ee
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-13 16:10:16 +0000

    Kommentare hinzugefügt

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/ICreateReleaseView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/ICreateReleaseView.java
@@ -5,15 +5,42 @@ import com.google.gwt.user.datepicker.client.DateBox;
 import fhdw.ipscrum.client.events.EventArgs;
 import fhdw.ipscrum.client.events.EventHandler;
 
+/**
+ * @author Phase II / Gruppe I
+ *
+ */
 public interface ICreateReleaseView extends IView{
 
+	/**
+	 * Method addSaveVersionHandler
+	 * Use this to register a handler for an event 
+	 * which saves a new version 
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
 	public abstract void addSaveVersionHandler(EventHandler<EventArgs> args);
 
+	/**
+	 * Method getReleaseVersion
+	 * Returns the version of the release
+	 * @return String
+	 */
 	public abstract String getReleaseVersion();
 
+	
+	/**
+	 * Method addCancelCreateReleaseHandler
+	 * Use this to register a handler for an event 
+	 * which cancels the creation of a new release
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
 	public abstract void addCancelCreateReleaseHandler(
 			EventHandler<EventArgs> args);
 
+	/**
+	 * Method getDateBox
+	 * Returns the DateBox
+	 * @return DateBox
+	 */
 	public abstract DateBox getDateBox();
 
 }
