c8c3ef477cf59e97cda2d5d0e93dab73961a8786
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-13 16:44:41 +0000

    Kommentare hinzugefügt

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/IReleaseView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/IReleaseView.java
@@ -5,16 +5,50 @@ import java.util.Vector;
 import fhdw.ipscrum.client.events.EventArgs;
 import fhdw.ipscrum.client.events.EventHandler;
 import fhdw.ipscrum.client.events.args.ReleaseArgs;
+import fhdw.ipscrum.shared.model.Release;
 import fhdw.ipscrum.shared.model.interfaces.IRelease;
 
+/**
+ * @author Phase II / Gruppe I
+ *
+ */
 public interface IReleaseView extends IView{
 
+	/**
+	 * Method addReleaseDetailsEventHandler
+	 * Use this to register a handler for an event 
+	 * which shows the details of a release 
+	 * @param args from Type {@link EventHandler<ReleaseArgs>}
+	 */
 	public abstract void addReleaseDetailsEventHandler(EventHandler<ReleaseArgs> arg);
+	
+	/**
+	 * Method addNewReleaseEventHandler
+	 * Use this to register a handler for an event 
+	 * which creates a new {@link Release} 
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
 	public abstract void addNewReleaseEventHandler(EventHandler<EventArgs> arg);
-	public abstract void addDeleteReleaseEventHandler(EventHandler<fhdw.ipscrum.client.events.args.ReleaseArgs> args);
-	public abstract void addReleaseSelectedEventHandler(EventHandler<fhdw.ipscrum.client.events.args.ReleaseArgs> args);
 	
+	/**
+	 * Method addDeleteReleaseEventHandler
+	 * Use this to register a handler for an event 
+	 * which deletes a new {@link Release} 
+	 * @param args from Type {@link EventHandler<ReleaseArgs>}
+	 */
+	public abstract void addDeleteReleaseEventHandler(EventHandler<ReleaseArgs> args);
 	
+	/**
+	 * Method addReleaseSelectedEventHandler
+	 * @param args from Type {@link EventHandler<ReleaseArgs>}
+	 */
+	public abstract void addReleaseSelectedEventHandler(EventHandler<ReleaseArgs> args);
+	
+	/**
+	 * Method refreshReleases
+	 * Refreshes releases
+	 * @param args from Type {@link Vector<IRelease>}
+	 */
 	public abstract void refreshReleases(Vector<IRelease> release);
 
 	
