a017de9bcb3dc3cf276416454a6040ead0715c83
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-13 18:31:39 +0000

    Kommentare hinzugefügt

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/ISprintView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/ISprintView.java
@@ -5,18 +5,35 @@ import java.util.Vector;
 import fhdw.ipscrum.client.events.EventArgs;
 import fhdw.ipscrum.client.events.EventHandler;
 import fhdw.ipscrum.client.events.args.SprintArgs;
+import fhdw.ipscrum.shared.model.Sprint;
 import fhdw.ipscrum.shared.model.interfaces.ISprint;
 
+/**
+ * @author Phase II / Gruppe I
+ *
+ */
 public interface ISprintView extends IView {
 
+	//wird hier nicht benötigt! (siehe IReleaseView)
 	public abstract void addNewReleaseEventHandler(EventHandler<EventArgs> arg);
 
-	void addSprintDetailsEventHandler(
-			EventHandler<SprintArgs> arg);
+	/**
+	 * Method addSprintDetailsEventHandler
+	 * Use this to register a handler for an event 
+	 * which shows the details of a {@link Sprint} 
+	 * @param args from Type {@link EventHandler<SprintArgs>}
+	 */
+	void addSprintDetailsEventHandler(EventHandler<SprintArgs> arg);
 
+	//wird hier nicht benötigt! (siehe IReleaseView)
 	void addDeleteReleaseEventHandler(
 			EventHandler<SprintArgs> arg);
 
+	/**
+	 * Method refreshSprints
+	 * Refreshes Sprints
+	 * @param args from Type {@link Vector<ISprint>}
+	 */
 	void refreshSprints(Vector<ISprint> sprints);
 	
 }
