260930251510e8e49e0669af7a1713cfc3b5646b
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-13 16:26:06 +0000

    Kommentare hinzugefügt

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/IProjectView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/IProjectView.java
@@ -8,19 +8,74 @@ import fhdw.ipscrum.client.events.EventArgs;
 import fhdw.ipscrum.client.events.EventHandler;
 import fhdw.ipscrum.client.events.args.ProjectEventArgs;
 import fhdw.ipscrum.shared.model.Project;
+import fhdw.ipscrum.client.view.ProductBacklogView;
+import fhdw.ipscrum.client.view.ReleaseView;
+import fhdw.ipscrum.client.view.SprintView;
 
+/**
+ * @author Phase II / Gruppe I
+ *
+ */
 public interface IProjectView extends IView{
-
-	public abstract void addNewProjectEventHandler(EventHandler<EventArgs> arg);
-	public abstract void addDeleteProjectEventHandler(EventHandler<ProjectEventArgs> arg);
-	public abstract void addProjectSelectionHandler(EventHandler<ProjectEventArgs> arg);
 	
+	/**
+	 * Method addNewProjectEventHandler
+	 * Use this to register a handler for an event 
+	 * which opens a new dialog to create a new {@link Project} 
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
+	public abstract void addNewProjectEventHandler(EventHandler<EventArgs> arg);
+	
+	/**
+	 * Method addDeleteProjectEventHandler
+	 * Use this to register a handler for an event 
+	 * which deletes a {@link Project} 
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
+	public abstract void addDeleteProjectEventHandler(EventHandler<ProjectEventArgs> arg);
+	
+	/**
+	 * Method addProjectSelectionEventHandler
+	 * Use this to register a handler for an event 
+	 * which opens the {@link ProductBacklogView}, the {@link ReleaseView} 
+	 * and the {@link SprintView} for the selected {@link Project}
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
+	public abstract void addProjectSelectionHandler(EventHandler<ProjectEventArgs> arg);
+
+	/**
+	 * Method getMasterProductBacklogPanel
+	 * Returns the MasterProductBacklogPanel
+	 * @return Panel
+	 */
 	public abstract Panel getMasterProductBackloglPanel();
+	
+	/**
+	 * Method getMasterReleasePanel
+	 * Returns the MasterReleasePanel
+	 * @return Panel
+	 */
 	public abstract Panel getMasterReleasePanel();
+	
+	/**
+	 * Method getMasterSprintPanel
+	 * Returns the MasterSprintPanel
+	 * @return Panel
+	 */
 	public abstract Panel getMasterSprintPanel();
+	
+	/**
+	 * Method getMasterSprintProductBacklogPanel
+	 * Returns the MasterSprintProductBacklogPanel
+	 * @return Panel
+	 */
 	public abstract Panel getMasterSprintProductBackloglPanel();
 	
 	
-	
+	/**
+	 * Method refreshProjects
+	 * Refreshes the projects
+	 * @param projects
+	 */
 	public abstract void refreshProjects(Vector<Project> projects);
 }
