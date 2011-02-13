88fb1e3a5fa658625e065c1708c89f350359c902
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-13 18:27:49 +0000

    Update Kommentare

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/ICreateProjectView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/ICreateProjectView.java
@@ -2,6 +2,7 @@ package fhdw.ipscrum.client.view.interfaces;
 
 import fhdw.ipscrum.client.events.EventArgs;
 import fhdw.ipscrum.client.events.EventHandler;
+import fhdw.ipscrum.shared.model.Project;
 
 
 /**
@@ -14,14 +15,14 @@ public interface ICreateProjectView extends IView{
 	/**
 	 * Method addSaveProjectHandler
 	 * Use this to register a handler for an event 
-	 * which creates a new project
+	 * which creates a new {@link Project)
 	 * @param args from Type {@link EventHandler<EventArgs>}
 	 */
 	public abstract void addSaveProjectHandler(EventHandler<EventArgs> args);
 	
 	/**
 	 * Method getProjectName
-	 * Returns the name of the project
+	 * Returns the name of the {@link Project}
 	 * @return String
 	 */
 	public abstract String getProjectName();
@@ -29,7 +30,7 @@ public interface ICreateProjectView extends IView{
 	
 	/**
 	 * Method setProjectName
-	 * Sets the name of the project
+	 * Sets the name of the {@link Project}
 	 * @param name String
 	 */
 	public abstract void setProjectName(String name);
@@ -38,7 +39,7 @@ public interface ICreateProjectView extends IView{
 	/**
 	 * Method addCancelCreateProjectHandler
 	 * Use this to register a handler for an event 
-	 * which cancels the creation of a new project
+	 * which cancels the creation of a new {@link Project}
 	 * @param args from Type {@link EventHandler<EventArgs>}
 	 */
 	public abstract void addCancelCreateProjectHandler(EventHandler<EventArgs> args);
