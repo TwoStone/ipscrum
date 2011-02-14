ed46f31f8853da92a37b1f4024d4cbab452b0481
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-14 15:41:57 +0000

    Kommentare hinzugefügt

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/IAddSprintToReleaseView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/IAddSprintToReleaseView.java
@@ -15,18 +15,52 @@ import fhdw.ipscrum.shared.model.interfaces.ISprint;
  *
  */
 public interface IAddSprintToReleaseView extends IView{
-
+	
+	
+	/**
+	 * Method getBtnSave
+	 * Returns SaveButton
+	 * @return Button
+	 */
 	public abstract Button getBtnSave();
-
+	
+	/**
+	 * Method getBtnAbort
+	 * Returns AbortButton
+	 * @return Button
+	 */
 	public abstract Button getBtnAbort();
 
+	/**
+	 * Method getLblRelease
+	 * Returns ReleaseLabel
+	 * @return Label
+	 */
 	public abstract Label getLblRelease();
 
+	/**
+	 * Method addSaveSprintsEventHandler
+	 * Use this to register a handler for an event
+	 * which saves sprints
+	 * @param args from Type {@link EventHandler<SprintArgs>}
+	 */
 	public abstract void addSaveSprintsEventHandler(EventHandler<SprintArgs> arg);
 
+	/**
+	 * Method addCancelAddSprintEventHandler
+	 * Use this to register a handler for an event
+	 * which cancels the addition of a {@Sprint}
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
 	public abstract void addCancelAddSprintViewEventHandler(
 			EventHandler<EventArgs> arg);
-
+	
+	/**
+	 * Method refreshSprints
+	 * Use this to register a handler for an event
+	 * which refreshes sprints
+	 * @param args from Type {@link Vector<ISprint>}
+	 */
 	public abstract void refreshSprints(Vector<ISprint> sprints);
 
 }
