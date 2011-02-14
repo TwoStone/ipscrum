e04f28ecd5a5ddbcc29fc7263675fb12b416cc4e
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-14 15:55:00 +0000

    Kommentare hinzugefügt

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/ISprintTableView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/ISprintTableView.java
@@ -7,12 +7,27 @@ import com.google.gwt.user.cellview.client.CellTable;
 import fhdw.ipscrum.shared.model.Sprint;
 import fhdw.ipscrum.shared.model.interfaces.ISprint;
 
+/**
+ * @author Phase II / Gruppe I
+ *
+ */
 public interface ISprintTableView {
 
+	/**
+	 * Method getCurrentlySelected
+	 * Returns the currently selected {@link Sprint}
+	 * @return Sprint
+	 */
 	public abstract Sprint getCurrentlySelected();
 
 	public abstract void refreshSprints(Vector<ISprint> sprints);
 
+	
+	/**
+	 * Method getTableSprint
+	 * Returns celltable with type {@link ISprint}
+	 * @return CellTable
+	 */
 	public abstract CellTable<ISprint> getTableSprint();
 
 }
