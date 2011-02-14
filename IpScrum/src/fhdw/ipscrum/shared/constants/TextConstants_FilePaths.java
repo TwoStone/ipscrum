742636c490afb82a38d23f6d59ada7051e9edebd
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-14 10:58:40 +0000

    Klasse für FilePath erstellt

---
+++ IpScrum/src/fhdw/ipscrum/shared/constants/TextConstants_FilePaths.java
@@ -0,0 +1,13 @@
+package fhdw.ipscrum.shared.constants;
+
+public class TextConstants_FilePaths {
+	
+	public static final String NEW_FILE_PATH = "images/newfile.png";
+	public static final String DETAILS_PATH = "images/details.png";
+	public static final String DELETE_PATH = "images/delete.png";
+	public static final String TOP_ARROW_PATH = "images/toparrow.png";
+	public static final String UP_ARROW_PATH = "images/uparrow.png";
+	public static final String DOWN_ARROW_PATH = "images/downarrow.png";
+	public static final String BOTTOM_ARROW_PATH = "images/bottomarrow.png";
+
+}
