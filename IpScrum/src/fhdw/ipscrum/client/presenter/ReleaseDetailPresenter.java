9c9d1fa17b038e6f055aa56431914f1acbcf475f
ha\hfw408wo <ha\hfw408wo@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-02-16 12:22:21 +0000

    Release entfernen hinzugefügt!

--- IpScrum/src/fhdw/ipscrum/client/presenter/ReleaseDetailPresenter.java
+++ IpScrum/src/fhdw/ipscrum/client/presenter/ReleaseDetailPresenter.java
@@ -6,87 +6,89 @@ import com.google.gwt.user.client.ui.Panel;
 import fhdw.ipscrum.client.events.EventArgs;
 import fhdw.ipscrum.client.events.EventHandler;
 import fhdw.ipscrum.client.events.args.SprintArgs;
-import fhdw.ipscrum.client.utils.GwtUtils;
 import fhdw.ipscrum.client.view.ReleaseDetailView;
 import fhdw.ipscrum.client.view.interfaces.IReleaseDetailView;
 import fhdw.ipscrum.shared.model.Release;
 
 public class ReleaseDetailPresenter extends Presenter<IReleaseDetailView> {
 
-	private Release release;
-	
-	public ReleaseDetailPresenter(Panel parent, Release release) {
+	private final Release release;
+
+	public ReleaseDetailPresenter(final Panel parent, final Release release) {
 		super(parent);
 		this.release = release;
 		this.initialize();
 	}
 
 	private void initialize() {
-	if (this.release.getSprints() != null){
-		this.getView().refreshSprints(this.release.getSprints());
-	}
+		if (this.release.getSprints() != null) {
+			this.getView().refreshSprints(this.release.getSprints());
+		}
 	}
 
 	@Override
 	protected IReleaseDetailView createView() {
-		
+
 		final IReleaseDetailView view = new ReleaseDetailView();
-		
-		view.addCancelReleaseDetailViewHandler(new EventHandler<EventArgs>(){
+
+		view.addCancelReleaseDetailViewHandler(new EventHandler<EventArgs>() {
 
 			@Override
-			public void onUpdate(Object sender, EventArgs eventArgs) {
+			public void onUpdate(final Object sender, final EventArgs eventArgs) {
 
 				ReleaseDetailPresenter.this.abort();
-				
+
 			}
 		});
-		
-		view.addAddSprintEventHandler(new EventHandler<EventArgs>(){
+
+		view.addAddSprintEventHandler(new EventHandler<EventArgs>() {
 
 			@Override
-			public void onUpdate(Object sender, EventArgs eventArgs) {
-			
-				
-				final DialogBox diaBox = new DialogBox();
-			
-				final AddSprintToReleasePresenter presenter = new AddSprintToReleasePresenter(diaBox, ReleaseDetailPresenter.this.release);
-			
-					presenter.getAborted().add(new EventHandler<EventArgs>() {
+			public void onUpdate(final Object sender, final EventArgs eventArgs) {
 
-								@Override
-								public void onUpdate(Object sender,
-										EventArgs eventArgs) {
-									diaBox.clear();
-									diaBox.hide();
-								}
-							});
-					
-					presenter.getFinished().add(new  EventHandler<EventArgs>() {
-						
-						@Override
-						public void onUpdate(Object sender, EventArgs eventArgs) {
-						
-							diaBox.clear();
-							diaBox.hide();
-							ReleaseDetailPresenter.this.initialize();
-						}
-					});						
-		diaBox.center();
-			}	
+				final DialogBox diaBox = new DialogBox();
+
+				final AddSprintToReleasePresenter presenter = new AddSprintToReleasePresenter(
+						diaBox, ReleaseDetailPresenter.this.release);
+
+				presenter.getAborted().add(new EventHandler<EventArgs>() {
+
+					@Override
+					public void onUpdate(final Object sender,
+							final EventArgs eventArgs) {
+						diaBox.clear();
+						diaBox.hide();
+					}
+				});
+
+				presenter.getFinished().add(new EventHandler<EventArgs>() {
+
+					@Override
+					public void onUpdate(final Object sender,
+							final EventArgs eventArgs) {
+
+						diaBox.clear();
+						diaBox.hide();
+						ReleaseDetailPresenter.this.initialize();
+					}
+				});
+				diaBox.center();
+			}
 		});
-		
+
 		view.addDeleteSprintEventHandler(new EventHandler<SprintArgs>() {
 
 			@Override
-			public void onUpdate(Object sender, SprintArgs eventArgs) {
-				GwtUtils.displayError("Entfernen von Sprints ist derzeit noch nicht vorgesehen!");
+			public void onUpdate(final Object sender, final SprintArgs eventArgs) {
+
+				ReleaseDetailPresenter.this.release.removeSprint(eventArgs
+						.getSprint());
+				ReleaseDetailPresenter.this.initialize();
+
 			}
 		});
-		
+
 		return view;
 	}
 
-
 }
-	
