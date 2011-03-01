960545d20d7986a9fe977bd97c93bd28183d8614
ha\hfw408hu <ha\hfw408hu@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-01 15:11:40 +0000

    sprint-table is now sorted

--- IpScrum/src/fhdw/ipscrum/client/view/widgets/charts/RegularReleaseBurndownChart.java
+++ IpScrum/src/fhdw/ipscrum/client/view/widgets/charts/RegularReleaseBurndownChart.java
@@ -1,10 +1,14 @@
 package fhdw.ipscrum.client.view.widgets.charts;
 
+import java.util.Comparator;
+import java.util.Vector;
+
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.googlecode.gchart.client.GChart;
 
 import fhdw.ipscrum.client.view.widgets.SprintTableView;
 import fhdw.ipscrum.shared.model.interfaces.IRelease;
+import fhdw.ipscrum.shared.model.interfaces.ISprint;
 
 public class RegularReleaseBurndownChart extends ReleaseBurndownChart {
 
@@ -18,7 +22,19 @@ public class RegularReleaseBurndownChart extends ReleaseBurndownChart {
 		verticalPanel.add(rbdChart);
 
 		SprintTableView sprintTable = new SprintTableView();
-		sprintTable.refreshSprints(this.release.getSprints());
+		sprintTable.refreshSprints(this.getSortedSprintList());
 		verticalPanel.add(sprintTable);
+	}
+
+	private Vector<ISprint> getSortedSprintList() {
+		Vector<ISprint> sortedSprints = new Vector<ISprint>();
+		sortedSprints.addAll(this.release.getSprints());
+		java.util.Collections.sort(sortedSprints, new Comparator<ISprint>() {
+			@Override
+			public int compare(ISprint o1, ISprint o2) {
+				return o1.getEnd().compareTo(o2.getEnd());
+			}
+		});
+		return sortedSprints;
 	}
 }
