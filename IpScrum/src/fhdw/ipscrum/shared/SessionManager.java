5234eb7ae01ed4ae07231baea1464bbbd5bfb62e
ha\hfw408wa <ha\hfw408wa@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-03 17:18:15 +0000

    Laden endgültig lauffähig!

--- IpScrum/src/fhdw/ipscrum/shared/SessionManager.java
+++ IpScrum/src/fhdw/ipscrum/shared/SessionManager.java
@@ -1,15 +1,23 @@
 package fhdw.ipscrum.shared;
 
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import fhdw.ipscrum.client.services.PersistenceService;
 import fhdw.ipscrum.client.utils.GwtUtils;
+import fhdw.ipscrum.shared.model.DemoModel;
 import fhdw.ipscrum.shared.model.Root;
 import fhdw.ipscrum.shared.model.interfaces.IPerson;
+import fhdw.ipscrum.shared.persistence.SerializationRoot;
 
 public class SessionManager {
 
+	public static interface LoadCallback {
+		void onLoaded();
+	}
+
 	public final static String IDENTIFIER = "RootModel";
+
 	private static SessionManager instance;
 
 	public static SessionManager getInstance() {
@@ -21,7 +29,7 @@ public class SessionManager {
 
 	private IPerson loginUser;
 
-	private final Root model;
+	private Root model;
 
 	private SessionManager() {
 		super();
@@ -36,19 +44,42 @@ public class SessionManager {
 		return this.model;
 	}
 
+	public void load(final LoadCallback callback) {
+
+		PersistenceService.Util.getInstance().load(IDENTIFIER,
+				new AsyncCallback<SerializationRoot>() {
+
+					@Override
+					public void onFailure(final Throwable caught) {
+						SessionManager.this.model = new DemoModel();
+						GwtUtils.displayError("Modell korrumpiert. Demo-Daten geladen.");
+						callback.onLoaded();
+					}
+
+					@Override
+					public void onSuccess(final SerializationRoot result) {
+						SessionManager.this.model = (Root) result;
+						callback.onLoaded();
+					}
+				});
+	}
+
+	public void loadDemoData() {
+		this.model = new DemoModel();
+	}
+
 	public void save() {
 		PersistenceService.Util.getInstance().save(this.model, IDENTIFIER,
 				new AsyncCallback<Void>() {
 
 					@Override
 					public void onFailure(final Throwable caught) {
-						caught.printStackTrace();
 						GwtUtils.displayError(caught.getMessage());
 					}
 
 					@Override
 					public void onSuccess(final Void result) {
-
+						Window.alert("Speichern erfolgreich!");
 					}
 				});
 	}
