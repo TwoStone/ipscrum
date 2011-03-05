9fed0b101af4a4e63266e5aae519f81a1773ce48
ha\hfw408wa <ha\hfw408wa@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-05 19:57:50 +0000

    Fehler behoben.

--- IpScrum/src/fhdw/ipscrum/client/presenter/SelectSystemPresenter.java
+++ IpScrum/src/fhdw/ipscrum/client/presenter/SelectSystemPresenter.java
@@ -11,7 +11,8 @@ import fhdw.ipscrum.client.view.interfaces.ISelectSystemView;
 import fhdw.ipscrum.shared.model.System;
 
 /**
- * presenter class for systems. this view is used to select systems of an amount of available systems.
+ * presenter class for systems. this view is used to select systems of an amount
+ * of available systems.
  */
 public class SelectSystemPresenter extends Presenter<ISelectSystemView> {
 
@@ -22,15 +23,21 @@ public class SelectSystemPresenter extends Presenter<ISelectSystemView> {
 	/**
 	 * Constructor for SelectSystemPresenter.
 	 * 
-	 * @param parent Panel
+	 * @param parent
+	 *            Panel
 	 * @param parentPresenter
 	 * @param availableSystems
 	 * @param selectedSystems
 	 */
-	public SelectSystemPresenter(final Panel parent, final Presenter<?> parentPresenter, final Collection<System> selectedSystems, final Collection<System> availableSystems) {
+	public SelectSystemPresenter(final Panel parent,
+			final Presenter<?> parentPresenter,
+			final Collection<System> selectedSystems,
+			final Collection<System> availableSystems) {
 		super(parent, parentPresenter);
 		this.availableSystems = availableSystems;
 		this.selectedSystems = selectedSystems;
+		this.updateGuiData();
+		this.setupEventHandlers();
 	}
 
 	/**
@@ -41,45 +48,49 @@ public class SelectSystemPresenter extends Presenter<ISelectSystemView> {
 	@Override
 	protected ISelectSystemView createView() {
 		this.concreteView = new SelectSystemView();
-		this.updateGuiData();
-		this.setupEventHandlers();
 		return this.concreteView;
 	}
 
 	/**
-	 * this is called to set up the behavior of all interaction widgets of this view.
+	 * this is called to set up the behavior of all interaction widgets of this
+	 * view.
 	 */
 	private void setupEventHandlers() {
 
-		this.concreteView.defineRemoveSelectedSystemEvent(new EventHandler<SystemArgs>() {
-			@Override
-			public void onUpdate(Object sender, SystemArgs eventArgs) {
-				selectedSystems.remove(eventArgs.getSystem());
-				SelectSystemPresenter.this.updateGuiData();
-			}
+		this.concreteView
+				.defineRemoveSelectedSystemEvent(new EventHandler<SystemArgs>() {
+					@Override
+					public void onUpdate(Object sender, SystemArgs eventArgs) {
+						SelectSystemPresenter.this.selectedSystems
+								.remove(eventArgs.getSystem());
+						SelectSystemPresenter.this.updateGuiData();
+					}
 
-		});
+				});
 
-		this.concreteView.defineAddSelectedSystemEvent(new EventHandler<SystemArgs>() {
+		this.concreteView
+				.defineAddSelectedSystemEvent(new EventHandler<SystemArgs>() {
 
-			@Override
-			public void onUpdate(Object sender, SystemArgs eventArgs) {
-				selectedSystems.add(eventArgs.getSystem());
-				SelectSystemPresenter.this.updateGuiData();
-			}
-		});
+					@Override
+					public void onUpdate(Object sender, SystemArgs eventArgs) {
+						SelectSystemPresenter.this.selectedSystems
+								.add(eventArgs.getSystem());
+						SelectSystemPresenter.this.updateGuiData();
+					}
+				});
 	}
 
 	/**
-	 * this is called to update or fill the entries in the gui-tables/tree-display.
+	 * this is called to update or fill the entries in the
+	 * gui-tables/tree-display.
 	 */
 	private void updateGuiData() {
-		this.concreteView.updateAvailableSystemData(availableSystems);
-		this.concreteView.updateSelectedSystemData(selectedSystems);
+		this.concreteView.updateAvailableSystemData(this.availableSystems);
+		this.concreteView.updateSelectedSystemData(this.selectedSystems);
 
 	}
 
 	public Collection<System> getSelectedSystems() {
-		return selectedSystems;
+		return this.selectedSystems;
 	}
 }
