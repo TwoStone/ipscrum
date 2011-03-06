e6d93e6a769c222554f22036cd1626489533660d
ha\hfw408pi <ha\hfw408pi@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-06 10:53:30 +0000

    Taskboard Model : Equals und Hashcode in den Klassen Task und AbstractTaskState implementiert

--- IpScrum/src/fhdw/ipscrum/shared/model/AbstractTaskState.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/AbstractTaskState.java
@@ -20,4 +20,31 @@ public abstract class AbstractTaskState implements ITaskState {
 		return this.myTask;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		// simple identity check, no further attributes needed, because
+		// a task always has one state.	
+		return this == (AbstractTaskState)obj;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result;
+		return result;
+		// no attributes to generate hashcode, state already checked in 
+		// indirectHashCode of class Task
+	}
+	
+	
+	
+	
+
 }
--- IpScrum/src/fhdw/ipscrum/shared/model/Task.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/Task.java
@@ -237,14 +237,87 @@ public class Task extends Observable implements ITask {
 
 	@Override
 	public int indirectHashCode() {
-		// TODO Auto-generated method stub
-		return 0;
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((assignedPBIs == null) ? 0 : assignedPBIs.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((planEffort == null) ? 0 : planEffort.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
 	}
 
 	@Override
 	public boolean indirectEquals(Object obj) {
-		// TODO Auto-generated method stub
-		return false;
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (!(obj instanceof Task)) {
+			return false;
+		}
+		Task other = (Task) obj;
+		if (assignedPBIs == null) {
+			if (other.assignedPBIs != null) {
+				return false;
+			}
+		} else if (!assignedPBIs.equals(other.assignedPBIs)) {
+			return false;
+		}
+		if (description == null) {
+			if (other.description != null) {
+				return false;
+			}
+		} else if (!description.equals(other.description)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (planEffort == null) {
+			if (other.planEffort != null) {
+				return false;
+			}
+		} else if (!planEffort.equals(other.planEffort)) {
+			return false;
+		}
+		if (state == null) {
+			if (other.state != null) {
+				return false;
+			}
+		} else if (!state.equals(other.state)) {
+			return false;
+		}
+		return true;
 	}
+
+	/* (non-Javadoc)
+	 * @see fhdw.ipscrum.shared.observer.Observable#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		return this.indirectEquals(obj);
+	}
+
+	/* (non-Javadoc)
+	 * @see fhdw.ipscrum.shared.observer.Observable#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return this.indirectHashCode();
+	}
+
+
+	
+	
 
 }
