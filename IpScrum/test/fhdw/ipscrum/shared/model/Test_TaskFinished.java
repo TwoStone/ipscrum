160918cc442b3b3b7df69f8666d6e09eef445311
ha\hfw408bo <ha\hfw408bo@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-07 05:03:13 +0000

    Tests der Geter Tests der Fehler noch nicht, da hier noch Klärungsbedarf beim Import der Klasse org.junit.rules.ExpectedException besteht, welche beim späteren Aufruf in der Testfallklasse noch Fehler wirft

--- IpScrum/test/fhdw/ipscrum/shared/model/Test_TaskFinished.java
+++ IpScrum/test/fhdw/ipscrum/shared/model/Test_TaskFinished.java
@@ -1,30 +1,106 @@
 package fhdw.ipscrum.shared.model;
 
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
 
-;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 
 public class Test_TaskFinished {
+	
+	private static Person per1 = null;
+	private static Person per2 = null;
+	private static Feature pbi1 = null;
+	private static Task t1 = null;
+	private static Sprint sprint = null;
+	private static Team team1 = null;
+	private static SprintBacklog sprintbl = null;
+	private static Project test = null;
+	private static ProductBacklog pbltest = null;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		t1 = new Task("Task 1", "Beschreibung");
+		per1 = new Person("Max", "Mustermann");
+		per2 = new Person("Karin","Katze");
+		test = new Project("Test");
+		pbltest = test.getBacklog();
+		pbi1 = new Feature("A", "Test1", pbltest);
+		team1 = new Team("Team");
+		sprint = new Sprint("Sprint", "Beschreibung", new Date(),new Date(), team1);
+		sprintbl = new SprintBacklog(sprint);
+		t1.setResponsibility(per1);
+		
+		pbltest.addItem(pbi1);
+		team1.addMember(per1);
+		team1.addMember(per2);
+		pbi1.setSprint(sprint);
+		
+		sprintbl.addTask(t1);
+		
+		t1.setResponsibility(per1);
+		t1.finish();
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@Before
-	public void setUp() throws Exception {
+	// --------------------------------------------------------------------------------------
+	// ------------------------------- Test of Functions ------------------------------------
+	// --------------------------------------------------------------------------------------
+	
+	@Test
+	/**
+	 * Getting Finished Date
+	 */
+	public void testGetFinishDate() throws Exception{
+		assertEquals(new Date(), t1.getFinishDate());
+	}
+	
+	@Test
+	/**
+	 * Getting the responsible Person
+	 */
+	public void testGetResponsiblePerson() throws Exception{
+		assertEquals(per1,t1.getResponsiblePerson());
+	}
+	
+	@Test
+	/**
+	 * Test on having a responsible Person
+	 */
+	public void testHasResponsiblePerson() throws Exception{
+		assertEquals(true,t1.hasResponsiblePerson());
+	}
+	
+	@Test
+	/**
+	 * Test on being finished
+	 */
+	public void testIsFinished() throws Exception{
+		assertEquals(true,t1.isFinished());
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+	
+	
+	// ----------------------------------------------------------------------------------------
+	// ----------------------------- Test of Exceptions ---------------------------------------
+	// ----------------------------------------------------------------------------------------
+	
+	// @Rule
+	// TODO: Warum kann org.junit.rules.ExpectedException.class nicht in den Sourcen gefunden werden? 
+	// public ExpectedException ee = new ExpectedException();
+	
+	
+
 
 }
