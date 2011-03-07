5f55166219544dba43bb503ac2791b8d1345084d
ha\hfw408st <ha\hfw408st@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-07 18:27:48 +0000

    zur Testclasse gemacht

--- IpScrum/test/fhdw/ipscrum/setUpTestData.java
+++ IpScrum/test/fhdw/ipscrum/setUpTestData.java
@@ -1,7 +1,42 @@
 package fhdw.ipscrum;
 
-public class setUpTestData {
-	
-	
 
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public abstract class setUpTestData {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+
+	@Before
+	public void setUp() throws Exception {
+//		this.timestamp = System.currentTimeMillis();
+//		
+//		this.konto1 = Konto.createKonto();
+//		this.konto2 = Konto.createKonto();
+//		this.konto3 = Konto.createKonto();
+//		this.konto4 = Konto.createKonto();
+//		this.konto5 = Konto.createKonto();
+//		
+//		this.konto1.setSaldo(Geldbetrag.createGeldbetrag(modelConstants.InitializationConstants.KontoInitialSaldo));
+//		this.konto2.setSaldo(Geldbetrag.createGeldbetrag(modelConstants.InitializationConstants.KontoInitialSaldo));
+//		this.konto3.setSaldo(Geldbetrag.createGeldbetrag(modelConstants.InitializationConstants.KontoInitialSaldo));
+//		this.konto4.setSaldo(Geldbetrag.createGeldbetrag(modelConstants.InitializationConstants.KontoInitialSaldo));
+//		this.konto5.setSaldo(Geldbetrag.createGeldbetrag(modelConstants.InitializationConstants.KontoInitialSaldo));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
 }
