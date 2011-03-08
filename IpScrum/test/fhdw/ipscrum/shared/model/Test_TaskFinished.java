0013d36d258d99eefdddd04e7436f93d38a9b33b
ha\hfw408bo <ha\hfw408bo@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-08 07:28:29 +0000

    Test_TaskFinished soweit fertiggestellt

--- IpScrum/test/fhdw/ipscrum/shared/model/Test_TaskFinished.java
+++ IpScrum/test/fhdw/ipscrum/shared/model/Test_TaskFinished.java
@@ -25,23 +25,25 @@ public class Test_TaskFinished {
 	private static SprintBacklog sprintbl = null;
 	private static Project test = null;
 	private static ProductBacklog pbltest = null;
+	private static Date ende = null;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		t1 = new Task("Task 1", "Beschreibung");
-		per1 = new Person("Max", "Mustermann");
-		per2 = new Person("Karin","Katze");
 		test = new Project("Test");
 		pbltest = test.getBacklog();
+		per1 = new Person("Max", "Mustermann");
+		per2 = new Person("Karin","Katze");
 		pbi1 = new Feature("A", "Test1", pbltest);
 		pbi2 = new Feature("B", "Test2", pbltest);
+		t1 = new Task("Task 1", "Beschreibung");
 		team1 = new Team("Team");
 		sprint = new Sprint("Sprint", "Beschreibung", new Date(),new Date(), team1);
-		sprintbl = new SprintBacklog(sprint);
-		t1.setResponsibility(per1);
-		
+		sprintbl = sprint.getSprintBacklog();
+		ende = new Date(2011-03-03);
+				
 		pbltest.addItem(pbi1);
 		pbltest.addItem(pbi2);
+		test.addSprint(sprint);
 		team1.addMember(per1);
 		team1.addMember(per2);
 		pbi1.setSprint(sprint);
@@ -50,7 +52,7 @@ public class Test_TaskFinished {
 		sprintbl.addTask(t1);
 		
 		t1.setResponsibility(per1);
-		t1.finish();
+		t1.finish(new Date(2011-03-03));
 	}
 
 	@AfterClass
@@ -66,7 +68,7 @@ public class Test_TaskFinished {
 	 * Getting Finished Date
 	 */
 	public void testGetFinishDate() throws Exception{
-		assertEquals(new Date(), t1.getFinishDate());
+		assertEquals(ende, t1.getFinishDate());
 	}
 	
 	@Test
@@ -104,8 +106,62 @@ public class Test_TaskFinished {
 	 * Test on adding a PBI
 	 */
 	public void testAddPBI() throws Exception{
-		 t1.addPBI(pbi2);
+		t1.addPBI(pbi2);
 	}
 	
-	// TODO: Addition of Testcases for all Exceptions
+	@Test(expected = ForbiddenStateException.class)
+	/**
+	 * Test on finishing a Task
+	 */
+	public void testFinish1() throws Exception{
+		t1.finish();
+	}
+	
+	@Test(expected = ForbiddenStateException.class)
+	/**
+	 * Test on finishing a Task
+	 */
+	public void testFinish2() throws Exception{
+		t1.finish(new Date());
+	}
+	
+	@Test(expected = ForbiddenStateException.class)
+	/**
+	 * Test on removing a PBI
+	 */
+	public void testRemovePBI() throws Exception{
+		t1.removePBI(pbi1);
+	}
+	
+	@Test(expected = ForbiddenStateException.class)
+	/**
+	 * Test on setting the description
+	 */
+	public void testSetDescription() throws Exception{
+		t1.setDescription("Beschreibung ändern");
+	}
+	
+	@Test(expected = ForbiddenStateException.class)
+	/**
+	 * Test on setting the name
+	 */
+	public void testSetName() throws Exception{
+		t1.setName("Task");
+	}
+	
+	@Test(expected = ForbiddenStateException.class)
+	/**
+	 * Test on setting the responsibility
+	 */
+	public void testSetResponsibility() throws Exception{
+		t1.setResponsibility(per2);
+	}
+	
+	@Test(expected = ForbiddenStateException.class)
+	/**
+	 * Test on setting the planned effort
+	 */
+	public void testSetPlanEffort() throws Exception{
+		t1.setPlanEffort(5);
+	}
 }
