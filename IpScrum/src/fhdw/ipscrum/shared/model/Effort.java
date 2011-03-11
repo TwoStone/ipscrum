6390ae8b2c940e3b6b9025af047a24a4481c3260
ha\hfw408hu <ha\hfw408hu@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-11 01:02:13 +0000

    effort.tostring() eingefügt

--- IpScrum/src/fhdw/ipscrum/shared/model/Effort.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/Effort.java
@@ -32,6 +32,12 @@ public class Effort implements Serializable {
 		this.value = value;
 	}
 
+
+	@Override
+	public String toString() {
+		return this.value.toString();
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
