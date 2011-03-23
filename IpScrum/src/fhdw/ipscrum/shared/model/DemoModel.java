c0669f68a885ec18488218e44b021b6777dc0d3f
ha\hfw408hu <ha\hfw408hu@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-03-23 09:17:53 +0000

    Anpassungen Demo-Daten JUnit-Fehler behoben

--- IpScrum/src/fhdw/ipscrum/shared/model/DemoModel.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/DemoModel.java
@@ -14,25 +14,18 @@ public class DemoModel {
 		final Root root = model;
 
 		// Initial Systems
-
 		final ISystem rootSystem = root.getSysManager().getSystems();
 		final System betriebssysteme = new System("Betriebssysteme", rootSystem);
 		final System browser = new System("Browser", rootSystem);
-		final System applicationServer = new System("ApplicationServer",
-				rootSystem);
+		final System applicationServer = new System("ApplicationServer", rootSystem);
 		final System anwendungen = new System("Anwendungen", rootSystem);
-
 		final System firefox = new System("Firefox", browser);
 		final System iexplorer = new System("InternetExplorer", browser);
-
 		final System windows = new System("Windows", betriebssysteme);
-
 		final System winxp = new System("XP", windows);
 		final System winvista = new System("Vista", windows);
 		final System win7 = new System("7", windows);
-
 		final System sap = new System("SAP", anwendungen);
-
 		final System saphr = new System("HR", sap);
 		final System sapbw = new System("BW", sap);
 
@@ -76,34 +69,54 @@ public class DemoModel {
 		pChris.addRole(roleDeveloper);
 		pChris.addRole(roleTSUser);
 
+		final Person pAngelina = new Person("Angelina", "Jung");
+		pAngelina.addRole(roleDeveloper);
+		pAngelina.addRole(roleTSUser);
+
+		final Person pTimo = new Person("Timo", "Hochwald");
+		pTimo.addRole(roleTester);
+		pTimo.addRole(roleDeveloper);
+		pTimo.addRole(roleTSUser);
+
+		final Person pStefan = new Person("Stefan", "Pietsch");
+		pStefan.addRole(roleDeveloper);
+		pStefan.addRole(roleTSUser);
+
 		root.addPerson(pSarah);
 		root.addPerson(pWilken);
 		root.addPerson(pChristin);
 		root.addPerson(pNils);
 		root.addPerson(pChris);
+		root.addPerson(pAngelina);
+		root.addPerson(pTimo);
+		root.addPerson(pStefan);
 
 		// Initial Teams
-		final Team team1 = new Team("Frontend");
-		team1.addMember(pSarah);
-		team1.addMember(pWilken);
+		final Team teamFrontend = new Team("Frontend");
+		teamFrontend.addMember(pSarah);
+		teamFrontend.addMember(pWilken);
 
-		final Team team2 = new Team("Backend");
-		team2.addMember(pChristin);
-		team2.addMember(pNils);
+		final Team teamBackend = new Team("Backend");
+		teamBackend.addMember(pChristin);
+		teamBackend.addMember(pNils);
 
-		final Team team3 = new Team("Reporting");
-		team3.addMember(pChris);
-		team3.addMember(pSarah);
-		team3.addMember(pWilken);
+		final Team teamReporting = new Team("Reporting");
+		teamReporting.addMember(pChris);
+		teamReporting.addMember(pSarah);
+		teamReporting.addMember(pWilken);
+		teamReporting.addMember(pNils);
+		teamReporting.addMember(pTimo);
+		teamReporting.addMember(pAngelina);
+		teamReporting.addMember(pStefan);
 
-		final Team team4 = new Team("Testing");
-		team4.addMember(pChris);
-		team4.addMember(pNils);
+		final Team teamTesting = new Team("Testing");
+		teamTesting.addMember(pChris);
+		teamTesting.addMember(pNils);
 
-		root.addTeam(team1);
-		root.addTeam(team2);
-		root.addTeam(team3);
-		root.addTeam(team4);
+		root.addTeam(teamFrontend);
+		root.addTeam(teamBackend);
+		root.addTeam(teamReporting);
+		root.addTeam(teamTesting);
 
 		// Initial Projects
 		final Project projekt1 = new Project("Projekt 1");
@@ -124,6 +137,7 @@ public class DemoModel {
 				CalendarUtils.getRandomReleaseDate(), projekt2);
 		final Release phase3 = new Release("Phase III", new Date(2011 - 1900,
 				3 - 1, 9), ipScrum);
+		final Release phase4 = new Release("Phase IV", new Date(2011 - 1900, 4 - 1, 8), ipScrum);
 
 		// Initial Sprints
 		final Date sprint1BeginDate = CalendarUtils.getRandomDateOfThisMonth();
@@ -131,14 +145,14 @@ public class DemoModel {
 				sprint1BeginDate, rel10.getReleaseDate());
 		final Sprint sprint1rel10 = new Sprint("Sprint1",
 				"Beschreibung Sprint 1", sprint1BeginDate, sprint1EndDate,
-				team1);
+				teamFrontend);
 
 		final Date sprint2BeginDate = CalendarUtils.getRandomDateOfThisMonth();
 		final Date sprint2EndDate = CalendarUtils.getRandomSprintEnddate(
 				sprint2BeginDate, rel10.getReleaseDate());
 		final Sprint sprint2rel10 = new Sprint("Sprint2",
 				"Beschreibung Sprint 2", sprint2BeginDate, sprint2EndDate,
-				team2);
+				teamBackend);
 
 		final Date sprint3BeginDate = new Date();
 		final Date sprint3EndDate = new Date();
@@ -146,7 +160,7 @@ public class DemoModel {
 		CalendarUtils.removeDaysFromDate(sprint3EndDate, 4);
 		final Sprint sprint3rel10 = new Sprint("Sprint3",
 				"Beschreibung Sprint 3", sprint3BeginDate, sprint3EndDate,
-				team1);
+				teamFrontend);
 
 		final Date sprint4BeginDate = new Date();
 		final Date sprint4EndDate = new Date();
@@ -154,7 +168,7 @@ public class DemoModel {
 		CalendarUtils.removeDaysFromDate(sprint4EndDate, 1);
 		final Sprint sprint4rel10 = new Sprint("Sprint4",
 				"Beschreibung Sprint 4", sprint4BeginDate, sprint4EndDate,
-				team1);
+				teamFrontend);
 
 		final Date sprint5BeginDate = new Date();
 		final Date sprint5EndDate = new Date();
@@ -162,62 +176,64 @@ public class DemoModel {
 		CalendarUtils.removeDaysFromDate(sprint5EndDate, 4);
 		final Sprint sprint5rel10 = new Sprint("Sprint5",
 				"Beschreibung Sprint 5", sprint5BeginDate, sprint5EndDate,
-				team2);
+				teamBackend);
 
 		final Date sprint6BeginDate = CalendarUtils.getRandomDateOfThisMonth();
 		final Date sprint6EndDate = CalendarUtils.getRandomSprintEnddate(
 				sprint6BeginDate, rel13.getReleaseDate());
 		final Sprint sprint6rel13 = new Sprint("Sprint6",
 				"Beschreibung Sprint 6", sprint6BeginDate, sprint6EndDate,
-				team2);
+				teamBackend);
 
 		final Date sprint7BeginDate = CalendarUtils.getRandomDateOfThisMonth();
 		final Date sprint7EndDate = CalendarUtils.getRandomSprintEnddate(
 				sprint7BeginDate, rel20.getReleaseDate());
 		final Sprint sprint7rel20 = new Sprint("Sprint7",
 				"Beschreibung Sprint 7", sprint7BeginDate, sprint7EndDate,
-				team2);
+				teamBackend);
 
 		final Date sprint8BeginDate = CalendarUtils.getRandomDateOfThisMonth();
 		final Date sprint8EndDate = CalendarUtils.getRandomSprintEnddate(
 				sprint8BeginDate, rel20.getReleaseDate());
 		final Sprint sprint8rel20 = new Sprint("Sprint8",
 				"Beschreibung Sprint 8", sprint8BeginDate, sprint8EndDate,
-				team2);
+				teamBackend);
 
 		final Date sprint9BeginDate = CalendarUtils.getRandomDateOfThisMonth();
 		final Date sprint9EndDate = CalendarUtils.getRandomSprintEnddate(
 				sprint9BeginDate, rel20.getReleaseDate());
 		final Sprint sprint9rel20 = new Sprint("Sprint9",
 				"Beschreibung Sprint 9", sprint9BeginDate, sprint9EndDate,
-				team2);
+				teamBackend);
 
 		final Date sprint10BeginDate = CalendarUtils.getRandomDateOfThisMonth();
 		final Date sprint10EndDate = CalendarUtils.getRandomSprintEnddate(
 				sprint10BeginDate, rel20.getReleaseDate());
 		final Sprint sprint10rel20 = new Sprint("Sprint10",
 				"Beschreibung Sprint 10", sprint10BeginDate, sprint10EndDate,
-				team2);
+				teamBackend);
 
 		final Sprint taskboardRelPhase3 = new Sprint("Taskboard",
 				"Bereitstellung von Task-Funktionalitäten", new Date(
 						2011 - 1900, 2 - 1, 14),
-						new Date(2011 - 1900, 3 - 1, 9), team1);
+						new Date(2011 - 1900, 3 - 1, 9), teamFrontend);
 		final Sprint reportingRelPhase3 = new Sprint("Reporting I",
 				"Bereitstellung von Statistikelementen", new Date(2011 - 1900,
-						2 - 1, 14), new Date(2011 - 1900, 3 - 1, 9), team3);
+						2 - 1, 14), new Date(2011 - 1900, 3 - 1, 9), teamReporting);
 		final Sprint ticketsRelPhase3 = new Sprint("Tickets II",
 				"Bereitstellung von Bugtracking-Funktionen", new Date(
 						2011 - 1900, 2 - 1, 14),
-						new Date(2011 - 1900, 3 - 1, 9), team2);
+						new Date(2011 - 1900, 3 - 1, 9), teamBackend);
+
+		final Sprint reporting2RelPhase4 = new Sprint("Reporting II",
+				"Projekthistorie, Sprintdetailerweiterungen und Velocitycharts", new Date(2011 - 1900,
+						3 - 1, 10), new Date(2011 - 1900, 4 - 1, 8), teamReporting);
 
 		// add possible Systems
 		projekt1.addPossibleSystem(anwendungen);
-
 		projekt2.addPossibleSystem(betriebssysteme);
 		projekt2.addPossibleSystem(browser);
 		projekt2.addPossibleSystem(applicationServer);
-
 		ipScrum.addPossibleSystem(betriebssysteme);
 		ipScrum.addPossibleSystem(browser);
 
@@ -238,6 +254,8 @@ public class DemoModel {
 		ipScrum.addSprint(reportingRelPhase3);
 		ipScrum.addSprint(ticketsRelPhase3);
 
+		ipScrum.addSprint(reporting2RelPhase4);
+
 		// assigning sprints to releases
 		rel10.addSprint(sprint1rel10);
 		rel10.addSprint(sprint2rel10);
@@ -254,6 +272,8 @@ public class DemoModel {
 		phase3.addSprint(taskboardRelPhase3);
 		phase3.addSprint(reportingRelPhase3);
 		phase3.addSprint(ticketsRelPhase3);
+
+		phase4.addSprint(reporting2RelPhase4);
 
 		root.addProject(projekt1);
 		root.addProject(projekt2);
@@ -336,6 +356,19 @@ public class DemoModel {
 		f11.setSprint(reportingRelPhase3);
 		f11.close();
 
+		final Feature f12 = new Feature("Projekthistorie", "Details zum Projektablauf", ipScrum.getBacklog());
+		f12.setLastEditor(pWilken);
+		f12.setManDayCosts(new Effort(15));
+		f12.setSprint(reporting2RelPhase4);
+		final Feature f13 = new Feature("Erweiterung Sprint-Details", "Mehr Detaileigenschaften im Sprint", ipScrum.getBacklog());
+		f13.setLastEditor(pWilken);
+		f13.setManDayCosts(new Effort(10));
+		f13.setSprint(reporting2RelPhase4);
+		final Feature f14 = new Feature("Velocitycharts", "Zur Analyse der Entwiklungsgeschwindigkeit", ipScrum.getBacklog());
+		f14.setLastEditor(pWilken);
+		f14.setManDayCosts(new Effort(15));
+		f14.setSprint(reporting2RelPhase4);
+
 		// adding features to projects
 		projekt1.getBacklog().addItem(f1);
 		projekt1.getBacklog().addItem(f2);
@@ -350,6 +383,9 @@ public class DemoModel {
 		ipScrum.getBacklog().addItem(f9);
 		ipScrum.getBacklog().addItem(f10);
 		ipScrum.getBacklog().addItem(f11);
+		ipScrum.getBacklog().addItem(f12);
+		ipScrum.getBacklog().addItem(f13);
+		ipScrum.getBacklog().addItem(f14);
 
 		// Initial Relations
 		RelationType.create("Abhängig von");
@@ -419,6 +455,60 @@ public class DemoModel {
 		taskReportPresenter.setPlanEffort(new Effort(1));
 		taskReportPresenter.setResponsibility(pChris);
 		taskReportPresenter.finish(new Date(2011 - 1900, 2 - 1, 17));
+
+
+		final Task taskTestdaten = new Task("Testdaten", "Demomodel aufräumen");
+		reporting2RelPhase4.getSprintBacklog().addTask(taskTestdaten);
+		taskTestdaten.addPBI(f12);
+		taskTestdaten.addPBI(f13);
+		taskTestdaten.addPBI(f14);
+		taskTestdaten.setPlanEffort(new Effort(8));
+		taskTestdaten.setResponsibility(pChris);
+		//		taskTestdaten.finish(new Date(2011 - 1900, 3 - 1, 23));
+
+		final Task taskChartAnpassungen = new Task("Implementierung Chart-Anpassungen", "Neuen Chart-Typ einführen");
+		reporting2RelPhase4.getSprintBacklog().addTask(taskChartAnpassungen);
+		taskChartAnpassungen.addPBI(f14);
+		taskChartAnpassungen.setPlanEffort(new Effort(10));
+		taskChartAnpassungen.setResponsibility(pWilken);
+		//		taskChartAnpassungen.finish(new Date(2011 - 1900, 3 - 1, 23));
+
+		final Task taskProjektHistorieModell = new Task("Model für Projekthistorie entwickeln", "PH-Modell");
+		reporting2RelPhase4.getSprintBacklog().addTask(taskProjektHistorieModell);
+		taskProjektHistorieModell.addPBI(f12);
+		taskProjektHistorieModell.setPlanEffort(new Effort(6));
+		taskProjektHistorieModell.setResponsibility(pStefan);
+		//		taskProjektHistorieModell.finish(new Date(2011 - 1900, 3 - 1, 23));
+
+		final Task taskProjektHistorieGUI = new Task("GUI für Projekthistorie entwickeln", "PH-GUI");
+		reporting2RelPhase4.getSprintBacklog().addTask(taskProjektHistorieGUI);
+		taskProjektHistorieGUI.addPBI(f12);
+		taskProjektHistorieGUI.setPlanEffort(new Effort(5));
+		taskProjektHistorieGUI.setResponsibility(pAngelina);
+		//		taskProjektHistorieGUI.finish(new Date(2011 - 1900, 3 - 1, 23));
+
+		final Task taskSprintDetailsModell = new Task("Model für SprintDetails erweitern", "Mehr Detaileigenschaften");
+		reporting2RelPhase4.getSprintBacklog().addTask(taskSprintDetailsModell);
+		taskSprintDetailsModell.addPBI(f13);
+		taskSprintDetailsModell.setPlanEffort(new Effort(4));
+		taskSprintDetailsModell.setResponsibility(pChris);
+		//		taskSprintDetailsModell.finish(new Date(2011 - 1900, 3 - 1, 23));
+
+		final Task taskSprintDetailsGUI = new Task("GUI für SprintDetails erweitern", "Mehr Detaileigenschaften");
+		reporting2RelPhase4.getSprintBacklog().addTask(taskSprintDetailsGUI);
+		taskSprintDetailsGUI.addPBI(f13);
+		taskSprintDetailsGUI.setPlanEffort(new Effort(3));
+		taskSprintDetailsGUI.setResponsibility(pAngelina);
+		//		taskSprintDetailsGUI.finish(new Date(2011 - 1900, 3 - 1, 23));
+
+		final Task taskGliederungsentwurf = new Task("Gliederungsentwurf", "Gliederung der Doku vorbereiten");
+		reporting2RelPhase4.getSprintBacklog().addTask(taskGliederungsentwurf);
+		taskGliederungsentwurf.addPBI(f12);
+		taskGliederungsentwurf.addPBI(f13);
+		taskGliederungsentwurf.addPBI(f14);
+		taskGliederungsentwurf.setPlanEffort(new Effort(4));
+		taskGliederungsentwurf.setResponsibility(pChris);
+		//		taskGliederungsentwurf.finish(new Date(2011 - 1900, 3 - 1, 23));
 
 	}
 }
