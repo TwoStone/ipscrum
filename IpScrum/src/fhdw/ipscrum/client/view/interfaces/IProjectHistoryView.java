ef5f0fb1e7651561025d4e82e1e3f2daeeccc82a
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-04-03 13:11:56 +0000

    Erweitert um "Sonstige Ereignisse"

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/IProjectHistoryView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/IProjectHistoryView.java
@@ -1,11 +1,37 @@
 package fhdw.ipscrum.client.view.interfaces;
 
-import java.util.Vector;
 
+import java.util.Date;
+import java.util.Vector;
+import fhdw.ipscrum.client.events.EventHandler;
+import fhdw.ipscrum.client.events.args.IncidentDetailArgs;
 import fhdw.ipscrum.shared.model.incidents.Incident;
+import fhdw.ipscrum.shared.model.interfaces.IPerson;
 
 public interface IProjectHistoryView extends IView {
+	
+	public void refreshPersons(Vector<IPerson> persons);
 
 	public void refreshProjectHistoryTable(Vector<Incident> incidents);
+
+	void addcreateIncidentHandler(EventHandler<IncidentDetailArgs> args);
+
+	public Date getStartDate();
+	
+	public Date getEndDate();
+	
+	public IPerson getPerson();
+
+	void addchangeTypHandler(EventHandler<IncidentDetailArgs> args);
+
+	public String getDescriptionText();
+
+	public void getOtherIssueView();
+
+	public void getNormalView();
+
+	public String getName();
+	
+	
 	
 }
