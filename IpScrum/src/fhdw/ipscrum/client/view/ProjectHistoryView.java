44746e1e9168c2ed305a33c67a21f40e0241b715
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-04-03 13:11:26 +0000

    Um "Sonstige Ereignisse" erweitert

--- IpScrum/src/fhdw/ipscrum/client/view/ProjectHistoryView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/ProjectHistoryView.java
@@ -2,32 +2,57 @@ package fhdw.ipscrum.client.view;
 
 import java.util.Date;
 import java.util.Vector;
-
+import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.cell.client.DateCell;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.datepicker.client.DateBox;
 import com.google.gwt.user.datepicker.client.DateBox.DefaultFormat;
-
+import com.google.gwt.view.client.SingleSelectionModel;
+import fhdw.ipscrum.client.events.Event;
+import fhdw.ipscrum.client.events.EventHandler;
+import fhdw.ipscrum.client.events.args.IncidentDetailArgs;
 import fhdw.ipscrum.client.view.interfaces.IProjectHistoryView;
+import fhdw.ipscrum.shared.constants.TextConstants;
 import fhdw.ipscrum.shared.model.incidents.Incident;
-import com.google.gwt.user.client.ui.ScrollPanel;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ValueBoxBase.TextAlignment;
+import fhdw.ipscrum.shared.model.interfaces.IPerson;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class ProjectHistoryView extends Composite implements IProjectHistoryView{
-	private CellTable<Object> projectHistoryTable;
+	private CellTable<Incident> projectHistoryTable;
+	private CellList<IPerson> personCellList;
+	private Button btnAnlegen;
+	private ListBox typListBox;
+	private DateBox startdateBox;
+	private DateBox enddateBox;
+	private TextArea descriptionTextArea;
+	
+	private final Event<IncidentDetailArgs> createIncident = new Event<IncidentDetailArgs>();
+	private final Event<IncidentDetailArgs> changeTyp = new Event<IncidentDetailArgs>();
+	private Label lblName;
+	private TextBox nameTextBox;
+	private Label lblBeschreibung;
+	private Label lblPersonen;
+	private ScrollPanel scrollPanel_1;
+	
 	public ProjectHistoryView() {
 		
 		AbsolutePanel concreteProjectHistoryPanel = new AbsolutePanel();
@@ -37,130 +62,273 @@ public class ProjectHistoryView extends Composite implements IProjectHistoryView
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
 		horizontalPanel.setStyleName("createFeatureTable");
 		horizontalPanel.setSpacing(3);
-		concreteProjectHistoryPanel.add(horizontalPanel, 60, 384);
-		horizontalPanel.setSize("500px", "150px");
+		concreteProjectHistoryPanel.add(horizontalPanel, 73, 394);
+		horizontalPanel.setSize("562px", "194px");
 		
 		VerticalPanel verticalPanel = new VerticalPanel();
 		verticalPanel.setSpacing(2);
 		horizontalPanel.add(verticalPanel);
-		verticalPanel.setSize("170px", "144px");
+		verticalPanel.setSize("170px", "188px");
 		
 		Label lblTyp = new Label("Typ:");
+		lblTyp.setStyleName("LabelElement");
 		verticalPanel.add(lblTyp);
+		lblTyp.setSize("95%", "22px");
 		
-		ListBox listBox = new ListBox();
-		listBox.setDirectionEstimator(false);
-		verticalPanel.add(listBox);
-		listBox.setSize("146px", "22px");
-		listBox.setVisibleItemCount(1);
+		typListBox = new ListBox();
+		typListBox.addChangeHandler(new ChangeHandler() {
+			public void onChange(ChangeEvent event) {
+				IncidentDetailArgs incidentDetails = new IncidentDetailArgs(ProjectHistoryView.this.typListBox.getItemText(ProjectHistoryView.this.typListBox.getSelectedIndex()));
+				ProjectHistoryView.this.changeTyp.fire(ProjectHistoryView.this, incidentDetails);
+			}
+		});
+		typListBox.addItem(TextConstants.INCIDENT_VACATION_NAME);
+		typListBox.addItem(TextConstants.INCIDENT_ILLNESS_NAME);
+		typListBox.addItem("Sonstiges Ereignis");
+		typListBox.setDirectionEstimator(false);
+		verticalPanel.add(typListBox);
+		typListBox.setSize("146px", "22px");
+		typListBox.setVisibleItemCount(1);
 		
 		Label lblStartdatum = new Label("Start-Datum:");
+		lblStartdatum.setStyleName("LabelElement");
 		verticalPanel.add(lblStartdatum);
 		
-		DateBox dateBox = new DateBox();
-		dateBox.setFormat(new DefaultFormat(DateTimeFormat.getFormat("dd-MM-yyyy")));
-		verticalPanel.add(dateBox);
+		startdateBox = new DateBox();
+		startdateBox.setFormat(new DefaultFormat(DateTimeFormat.getFormat("dd-MM-yyyy")));
+		verticalPanel.add(startdateBox);
 		
 		Label lblEndedatum = new Label("Ende-Datum:");
+		lblEndedatum.setStyleName("LabelElement");
 		verticalPanel.add(lblEndedatum);
 		
-		DateBox dateBox_1 = new DateBox();
-		dateBox_1.setFormat(new DefaultFormat(DateTimeFormat.getFormat("dd-MM-yyyy")));
-		verticalPanel.add(dateBox_1);
+		enddateBox = new DateBox();
+		enddateBox.setFormat(new DefaultFormat(DateTimeFormat.getFormat("dd-MM-yyyy")));
+		verticalPanel.add(enddateBox);
+		
+		
+		
+		btnAnlegen = new Button("Anlegen");
+		verticalPanel.add(btnAnlegen);
+		btnAnlegen.setStyleName("taskboardButton");
+		this.btnAnlegen.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				IncidentDetailArgs incidentDetails = new IncidentDetailArgs(ProjectHistoryView.this.typListBox.getItemText(ProjectHistoryView.this.typListBox.getSelectedIndex()));
+				ProjectHistoryView.this.createIncident.fire(ProjectHistoryView.this, incidentDetails);
+			}
+		});
 		
 		VerticalPanel verticalPanel_1 = new VerticalPanel();
+		verticalPanel_1.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
 		verticalPanel_1.setSpacing(2);
 		horizontalPanel.add(verticalPanel_1);
-		verticalPanel_1.setSize("200px", "144px");
+		verticalPanel_1.setSize("193px", "185px");
 		
-		Label lblBeschreibung = new Label("Beschreibung:");
-		verticalPanel_1.add(lblBeschreibung);
+		lblPersonen = new Label("Personen:");
+		verticalPanel_1.add(lblPersonen);
+		lblPersonen.setStyleName("LabelElement");
+		lblPersonen.setSize("95%", "22px");
 		
-		TextBox textBox = new TextBox();
-		textBox.setAlignment(TextAlignment.LEFT);
-		verticalPanel_1.add(textBox);
-		verticalPanel_1.setCellHorizontalAlignment(textBox, HasHorizontalAlignment.ALIGN_CENTER);
-		textBox.setSize("218px", "70px");
+		scrollPanel_1 = new ScrollPanel();
+		verticalPanel_1.add(scrollPanel_1);
+		scrollPanel_1.setStyleName("smallborder");
+		scrollPanel_1.setSize("187px", "100px");
+		
+		personCellList = new CellList<IPerson>(new AbstractCell<IPerson>(){
+			@Override
+			public void render(Context context, IPerson value, SafeHtmlBuilder sb) {
+				sb.appendEscaped(value.getFirstname() + " " + value.getLastname());
+			}
+		});
+		scrollPanel_1.setWidget(personCellList);
+		personCellList.setSize("172px", "95%");
 		
 		VerticalPanel verticalPanel_2 = new VerticalPanel();
 		verticalPanel_2.setSpacing(2);
 		horizontalPanel.add(verticalPanel_2);
-		verticalPanel_2.setSize("200px", "144px");
+		verticalPanel_2.setSize("185px", "185px");
 		
-		Label lblPersonen = new Label("Personen:");
-		verticalPanel_2.add(lblPersonen);
-		lblPersonen.setSize("226", "18");
+		lblName = new Label("Name:");
+		verticalPanel_2.add(lblName);
+		lblName.setStyleName("LabelElement");
+		lblName.setSize("95%", "22px");
 		
-		ListBox listBox_1 = new ListBox();
-		verticalPanel_2.add(listBox_1);
-		verticalPanel_2.setCellVerticalAlignment(listBox_1, HasVerticalAlignment.ALIGN_MIDDLE);
-		listBox_1.setSize("200px", "77px");
-		listBox_1.setVisibleItemCount(5);
+		nameTextBox = new TextBox();
+		verticalPanel_2.add(nameTextBox);
 		
-		Button btnAnlegen = new Button("Anlegen");
-		btnAnlegen.setStyleName("taskboardButton");
-		verticalPanel_2.add(btnAnlegen);
+		lblBeschreibung = new Label("Beschreibung:");
+		verticalPanel_2.add(lblBeschreibung);
+		lblBeschreibung.setStyleName("LabelElement");
+		lblBeschreibung.setSize("95%", "22px");
+		
+		descriptionTextArea = new TextArea();
+		verticalPanel_2.add(descriptionTextArea);
+		descriptionTextArea.setSize("178px", "70px");
+		descriptionTextArea.setVisible(false);
+		lblBeschreibung.setVisible(false);
+		nameTextBox.setVisible(false);
+		lblName.setVisible(false);
 		
 		HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
-		horizontalPanel_1.setSpacing(3);
-		concreteProjectHistoryPanel.add(horizontalPanel_1, 0, 0);
-		horizontalPanel_1.setSize("760px", "368px");
+		horizontalPanel_1.setSpacing(5);
+		concreteProjectHistoryPanel.add(horizontalPanel_1, 10, 24);
+		horizontalPanel_1.setSize("760px", "364px");
 		
 		ScrollPanel scrollPanel = new ScrollPanel();
 		horizontalPanel_1.add(scrollPanel);
-		scrollPanel.setSize("760px", "362px");
+		scrollPanel.setSize("735px", "344px");
 		
-		projectHistoryTable = new CellTable<Object>();
+		projectHistoryTable = new CellTable<Incident>();
+		projectHistoryTable.setFocus(false);
+		projectHistoryTable.setTabIndex(20);
 		scrollPanel.setWidget(projectHistoryTable);
 		projectHistoryTable.setTableLayoutFixed(false);
-		projectHistoryTable.setSize("760px", "360px");
+		projectHistoryTable.setSize("100%", "100%");
 		
-		Column<Object, Date> startDateColumn = new Column<Object, Date>(new DateCell()) {
+		Column<Incident, Date> startDateColumn = new Column<Incident, Date>(new DateCell()) {
 			@Override
-			public Date getValue(Object object) {
-				return null;
+			public Date getValue(Incident incident) {
+				return incident.getStart();
 			}
 		};
 		projectHistoryTable.addColumn(startDateColumn, "Start-Datum");
 		
-		Column<Object, Date> endDateColumn = new Column<Object, Date>(new DateCell()) {
+		Column<Incident, Date> endDateColumn = new Column<Incident, Date>(new DateCell()) {
 			@Override
-			public Date getValue(Object object) {
-				return null;
+			public Date getValue(Incident incident) {
+				return incident.getEnd();
 			}
 		};
 		projectHistoryTable.addColumn(endDateColumn, "Ende-Datum");
 		
-		TextColumn<Object> textColumn = new TextColumn<Object>() {
+		TextColumn<Incident> typColumn = new TextColumn<Incident>() {
 			@Override
-			public String getValue(Object object) {
-				return object.toString();
+			public String getValue(Incident incident) {
+				return incident.getName();
 			}
 		};
-		projectHistoryTable.addColumn(textColumn, "Typ");
+		projectHistoryTable.addColumn(typColumn, "Typ");
 		
-		TextColumn<Object> textColumn_1 = new TextColumn<Object>() {
+		TextColumn<Incident> descriptionColumn = new TextColumn<Incident>() {
 			@Override
-			public String getValue(Object object) {
-				return object.toString();
+			public String getValue(Incident incident) {
+				return incident.getDescription();
 			}
 		};
-		projectHistoryTable.addColumn(textColumn_1, "Beschreibung");
+		projectHistoryTable.addColumn(descriptionColumn, "Beschreibung");
 		
-		TextColumn<Object> textColumn_2 = new TextColumn<Object>() {
+		TextColumn<Incident> personColumn = new TextColumn<Incident>() {
 			@Override
-			public String getValue(Object object) {
-				return object.toString();
+			public String getValue(Incident incident) {
+				return incident.getParticipants().toString();
 			}
 		};
-		projectHistoryTable.addColumn(textColumn_2, "Personen");
+		projectHistoryTable.addColumn(personColumn, "Personen");
 
 	}
-	protected CellTable getProjectHistoryTable() {
+	protected CellTable<Incident> getProjectHistoryTable() {
 		return projectHistoryTable;
 	}
 	@Override
 	public void refreshProjectHistoryTable(Vector<Incident> incidents) {
 		this.getProjectHistoryTable().setRowData(incidents);
 	}
+	
+	@Override
+	public void refreshPersons(Vector<IPerson> persons){
+		this.getPersonCellList().setRowData(persons);
+		this.getPersonCellList().setSelectionModel(new SingleSelectionModel<IPerson>());
+	}
+	
+	@Override
+	public void addcreateIncidentHandler(EventHandler<IncidentDetailArgs> args) {
+		this.createIncident.add(args);
+	}
+	
+	@Override
+	public void addchangeTypHandler(EventHandler<IncidentDetailArgs> args) {
+		this.changeTyp.add(args);
+	}
+	
+	
+	public CellList<IPerson> getPersonCellList() {
+		return personCellList;
+	}
+	protected Button getBtnAnlegen() {
+		return btnAnlegen;
+	}
+	protected ListBox getTypListBox() {
+		return typListBox;
+	}
+	protected DateBox getStartdateBox() {
+		return startdateBox;
+	}
+	protected DateBox getEnddateBox() {
+		return enddateBox;
+	}
+	protected TextArea getDescriptionTextArea() {
+		return descriptionTextArea;
+	}
+
+	@Override
+	public Date getStartDate() {
+		return this.getStartdateBox().getValue();
+	}
+	@Override
+	public Date getEndDate() {
+		return this.getEnddateBox().getValue();
+	}
+	@Override
+	public String getDescriptionText() {
+		return this.getDescriptionTextArea().getValue();
+	}
+	@Override
+	public String getName() {
+		return this.getNameTextBox().getValue();
+	}
+
+
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public IPerson getPerson() {
+		return ((SingleSelectionModel<IPerson>)this.getPersonCellList().getSelectionModel()).getSelectedObject();
+	}
+	protected Label getLblName() {
+		return lblName;
+	}
+	protected TextBox getNameTextBox() {
+		return nameTextBox;
+	}
+	protected Label getLblBeschreibung() {
+		return lblBeschreibung;
+	}
+	protected Label getLblPersonen() {
+		return lblPersonen;
+	}
+	protected ScrollPanel getScrollPanel_1() {
+		return scrollPanel_1;
+	}
+	
+	@Override
+	public void getOtherIssueView(){
+	getDescriptionTextArea().setVisible(true);
+	getLblBeschreibung().setVisible(true);
+	getLblName().setVisible(true);
+	getNameTextBox().setVisible(true);
+	getLblPersonen().setVisible(false);
+	getPersonCellList().setVisible(false);
+	getScrollPanel_1().setVisible(false);
+}
+	@Override
+	public void getNormalView(){
+		getDescriptionTextArea().setVisible(false);
+		getLblBeschreibung().setVisible(false);
+		getLblName().setVisible(false);
+		getNameTextBox().setVisible(false);
+		getLblPersonen().setVisible(true);
+		getPersonCellList().setVisible(true);
+		getScrollPanel_1().setVisible(true);
+}
 }
