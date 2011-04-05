5d07ab273d4599697a5c237044f328dbe7d9e265
ha\hfw408wo <ha\hfw408wo@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-04-05 13:55:31 +0000

    ProjectCriterion muss ISearchCriterion erweitern!

--- IpScrum/src/fhdw/ipscrum/shared/model/search/criteria/ProjectCriterion.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/search/criteria/ProjectCriterion.java
@@ -1,8 +1,10 @@
 package fhdw.ipscrum.shared.model.search.criteria;
 
+import fhdw.ipscrum.shared.model.search.ISearchCriteria;
+
 /**
  * Represents the search criterion Project.
  */
-public interface ProjectCriterion {
+public interface ProjectCriterion extends ISearchCriteria {
 
 }
