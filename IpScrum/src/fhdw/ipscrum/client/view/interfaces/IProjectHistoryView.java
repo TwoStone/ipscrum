2d67ca408daedf33c8a2d902ac75ca7ace429d5c
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-04-06 23:19:41 +0000

    Erweitert

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/IProjectHistoryView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/IProjectHistoryView.java
@@ -1,44 +1,30 @@
 package fhdw.ipscrum.client.view.interfaces;
 
 
-import java.util.Date;
 import java.util.Set;
 import java.util.Vector;
+
 import fhdw.ipscrum.client.events.EventArgs;
 import fhdw.ipscrum.client.events.EventHandler;
 import fhdw.ipscrum.shared.model.incidents.Incident;
-import fhdw.ipscrum.shared.model.interfaces.IPerson;
+import fhdw.ipscrum.shared.model.incidents.IncidentType;
 
 public interface IProjectHistoryView extends IView {
-	
-	public void refreshPersons(Vector<IPerson> persons);
-
-	public void refreshProjectHistoryTable(Vector<Incident> incidents);
 
 	void addcreateIncidentHandler(EventHandler<EventArgs> args);
 
-	public Date getStartDate();
+	Set<IncidentType> getTypes();
 	
-	public Date getEndDate();
-	
-	public IPerson getPerson();
-
 	void addchangeTypHandler(EventHandler<EventArgs> args);
-
-	public String getDescriptionText();
-
-	public void getOtherIssueView();
-
-	public void getNormalView();
-
-	public String getName();
-
-	public Set<IPerson> getPersons();
-
-	public void initializeView();
-
-	public String getType();
 	
+	void refreshTypes(Vector<IncidentType> types);
+	
+	public void addcreateTypeHandler(EventHandler<EventArgs> args);
+
+
+	void refreshProjectHistoryTable(Vector<Incident> incidents);
+
+	void addShowIncidentsHandler(EventHandler<EventArgs> args);
 	
 	
 }
