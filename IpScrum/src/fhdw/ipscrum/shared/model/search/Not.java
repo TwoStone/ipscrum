dafc1ec938b1954f88f17b6c391c400c9ef6a39f
ha\hfw408wa <ha\hfw408wa@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-04-07 21:13:40 +0000

    Konstuktor sichbarkeit auf öffentlich

--- IpScrum/src/fhdw/ipscrum/shared/model/search/Not.java
+++ IpScrum/src/fhdw/ipscrum/shared/model/search/Not.java
@@ -21,8 +21,7 @@ public class Not extends SingleLogicSearchOperator {
 	/**
 	 * See {@link SingleLogicSearchOperator}
 	 */
-	@SuppressWarnings("unused")
-	private Not() {
+	public Not() {
 		super();
 	}
 
