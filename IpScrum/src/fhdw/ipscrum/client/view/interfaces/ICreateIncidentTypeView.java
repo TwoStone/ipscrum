28fabd6d7bfaaa3371f534919b4cf7950842c113
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-04-08 14:53:03 +0000

    Kommentare ergänzt

--- IpScrum/src/fhdw/ipscrum/client/view/interfaces/ICreateIncidentTypeView.java
+++ IpScrum/src/fhdw/ipscrum/client/view/interfaces/ICreateIncidentTypeView.java
@@ -3,12 +3,35 @@ package fhdw.ipscrum.client.view.interfaces;
 import fhdw.ipscrum.client.events.EventArgs;
 import fhdw.ipscrum.client.events.EventHandler;
 
+/**
+ * 
+ * @author Phase IV - Group Reporting II 
+ * 
+ */
 public interface ICreateIncidentTypeView extends IView {
 
+	/**
+	 * Returns the name
+	 * 
+	 * @return String
+	 */
 	String getName();
 	
+	/**
+	 *
+	 * Use this to register a handler for an event 
+	 * which creates an {@link IncidentType)
+	 * 
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
 	public void addCreateTypeHandler(EventHandler<EventArgs> args);
 
+	/**
+	 *
+	 * Use this to register a handler for an cancel event 
+	 * 
+	 * @param args from Type {@link EventHandler<EventArgs>}
+	 */
 	public void addCancelEventHandler(EventHandler<EventArgs> args);
 	
 }
