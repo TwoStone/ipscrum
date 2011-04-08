7cf126e6399eb9c950b6750af409d7beec88efce
ha\hfw408ju <ha\hfw408ju@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-04-08 16:45:39 +0000

    Kommentare ergänzt

--- IpScrum/src/fhdw/ipscrum/client/presenter/CreateIncidentTypePresenter.java
+++ IpScrum/src/fhdw/ipscrum/client/presenter/CreateIncidentTypePresenter.java
@@ -7,18 +7,35 @@ import fhdw.ipscrum.client.events.EventHandler;
 import fhdw.ipscrum.client.utils.GwtUtils;
 import fhdw.ipscrum.client.view.CreateIncidentTypeView;
 import fhdw.ipscrum.client.view.interfaces.ICreateIncidentTypeView;
+import fhdw.ipscrum.shared.constants.TextConstants;
 import fhdw.ipscrum.shared.exceptions.DoubleDefinitionException;
 import fhdw.ipscrum.shared.model.incidents.IncidentType;
 
+/**
+ * Presenter for creating new {@link IncidentType}
+ * 
+ * @author Phase IV - Group Reporting II
+ * 
+ */
 public class CreateIncidentTypePresenter extends
 		Presenter<ICreateIncidentTypeView> {
 
+	/**
+	 * Creates a new instance of {@link CreateIncidentTypePresenter}
+	 * 
+	 * @param parent
+	 * @param parentPresenter
+	 */
 	public CreateIncidentTypePresenter(Panel parent,
 			Presenter<?> parentPresenter) {
 		super(parent, parentPresenter);
 		this.addHandler();
 	}
 
+	/**
+	 * Adds the Handler for an event which creates a new {@link IncidentType}
+	 * and the Handler for an cancel event
+	 */
 	private void addHandler() {
 		this.getView().addCreateTypeHandler(new EventHandler<EventArgs>() {
 
@@ -26,8 +43,8 @@ public class CreateIncidentTypePresenter extends
 			public void onUpdate(Object sender, EventArgs eventArgs) {
 
 				if (CreateIncidentTypePresenter.this.getView().getName()
-						.equals("")) {
-					GwtUtils.displayWarning("Bitte einen Namen eintragen!");
+						.equals(TextConstants.EMPTY_TEXT)) {
+					GwtUtils.displayWarning(TextConstants.INCIDENT_WARNING_4);
 				} else {
 					try {
 						CreateIncidentTypePresenter.this
