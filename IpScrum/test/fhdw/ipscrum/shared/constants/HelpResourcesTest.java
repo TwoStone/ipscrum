0380bafd54d4e9c26ada7df7b52f2f3034fc5ed9
hfw408st <hfw408st@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-09-27 09:54:44 +0000

    TestKlasse für HelpResources erstellt

---
+++ IpScrum/test/fhdw/ipscrum/shared/constants/HelpResourcesTest.java
@@ -0,0 +1,21 @@
+package fhdw.ipscrum.shared.constants;
+
+
+/**
+ * This is to test the util-operations.
+ */
+public class HelpResourcesTest {
+
+	/**
+	 * tests the Method "getClassName" with a View-Class.
+	 */
+	// @Test
+	// public void testGetPresenterHelp1() {
+	//
+	// final Presenter presenter = new TaskboardPresenter(null, null, null);
+	//
+	// final String result = HelpResources.getPresenterHelp(presenter);
+	//
+	// Assert.assertEquals(result, HelpResources.TASKBOARDPRESENTER);
+	// }
+}
