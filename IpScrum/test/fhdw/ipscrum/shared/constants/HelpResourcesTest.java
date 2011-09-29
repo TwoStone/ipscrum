e855e5222550f46d1bdb251bc9eabdccaad3ed35
hfw408pi <hfw408pi@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-09-29 16:15:17 +0000

    Testfelle

--- IpScrum/test/fhdw/ipscrum/shared/constants/HelpResourcesTest.java
+++ IpScrum/test/fhdw/ipscrum/shared/constants/HelpResourcesTest.java
@@ -5,6 +5,9 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import fhdw.ipscrum.client.architecture.presenter.Presenter;
+import fhdw.ipscrum.client.architecture.view.IView;
+import fhdw.ipscrum.client.presenter.PersonRolePresenter;
+import fhdw.ipscrum.client.presenter.StateFieldAndTickettypeAdministrationPresenter;
 import fhdw.ipscrum.client.presenter.TaskboardPresenter;
 
 /**
@@ -24,4 +27,67 @@ public class HelpResourcesTest {
 
 		Assert.assertEquals(HelpResources.TASKBOARDPRESENTER, result);
 	}
+
+	/**
+	 * Tests getPresenterHelp with a Presenter with a class name greater than 32 characters.
+	 */
+	@Test
+	public void testGetPresenterHelpGT32() {
+		final Presenter presenter = new StateFieldAndTickettypeAdministrationPresenter(null);
+
+		final String result = HelpResources.getPresenterHelp(presenter);
+
+		Assert.assertEquals(HelpResources.STATEFIELDANDTICKETTYPEADMINISTR, result);
+	}
+
+	/**
+	 * Tests getPresenterHelp with a Presenter with a class name lower than 32 characters.
+	 */
+	@Test
+	public void testGetPresenterHelpLT32() {
+		final Presenter presenter = new PersonRolePresenter(null);
+
+		final String result = HelpResources.getPresenterHelp(presenter);
+
+		Assert.assertEquals(HelpResources.PERSONROLEPRESENTER, result);
+
+	}
+
+	/**
+	 * Tests getPresenterHelp with a Presenter which hasn't a assigned Help file.
+	 */
+	@Test
+	public void testGetPresenterHelp404() {
+		final Presenter presenter = new Presenter(null) {
+
+			@Override
+			public void updateView() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onModelUpdate() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public String getName() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public IView doGetView() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+
+		final String result = HelpResources.getPresenterHelp(presenter);
+
+		// Assert.assertEquals(HelpResources., result);
+	}
+
 }
