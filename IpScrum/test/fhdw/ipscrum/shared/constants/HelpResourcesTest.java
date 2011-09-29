477c2f6149e6f5a5909cdb617684ed9dc81ae1dc
hfw408pi <hfw408pi@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-09-29 15:12:53 +0000

    Testfall einkommentiert.

--- IpScrum/test/fhdw/ipscrum/shared/constants/HelpResourcesTest.java
+++ IpScrum/test/fhdw/ipscrum/shared/constants/HelpResourcesTest.java
@@ -1,5 +1,11 @@
 package fhdw.ipscrum.shared.constants;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import fhdw.ipscrum.client.architecture.presenter.Presenter;
+import fhdw.ipscrum.client.presenter.TaskboardPresenter;
 
 /**
  * This is to test the util-operations.
@@ -9,13 +15,13 @@ public class HelpResourcesTest {
 	/**
 	 * tests the Method "getPresenterHelp".
 	 */
-	// @Test
-	// public void testGetPresenterHelp1() {
-	//
-	// final Presenter presenter = new TaskboardPresenter(null, null, null);
-	//
-	// final String result = HelpResources.getPresenterHelp(presenter);
-	//
-	// Assert.assertEquals(result, HelpResources.TASKBOARDPRESENTER);
-	// }
+	@Test
+	public void testGetPresenterHelp1() {
+
+		final Presenter presenter = new TaskboardPresenter(null, null, null);
+
+		final String result = HelpResources.getPresenterHelp(presenter);
+
+		Assert.assertEquals(HelpResources.TASKBOARDPRESENTER, result);
+	}
 }
