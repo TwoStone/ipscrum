ccf4c1b0924942b223ec640388360c247e136eab
fhdwcsh <fhdwcsh@c396912c-af04-4d54-82ff-c7647759d7b0>
2011-10-14 22:06:05 +0000

    Testfall korrigiert bzw. implementiert

--- IpScrum/test/fhdw/ipscrum/shared/commands/metamodel/ListFieldAddValueCommandTest.java
+++ IpScrum/test/fhdw/ipscrum/shared/commands/metamodel/ListFieldAddValueCommandTest.java
@@ -3,24 +3,22 @@ package fhdw.ipscrum.shared.commands.metamodel;
 import java.io.File;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import fhdw.ipscrum.client.IDGenerator;
 import fhdw.ipscrum.server.ServerContext;
 import fhdw.ipscrum.server.TestUtils;
-import fhdw.ipscrum.shared.exceptions.model.ConsistencyException;
-import fhdw.ipscrum.shared.exceptions.model.DoubleDefinitionException;
-import fhdw.ipscrum.shared.exceptions.model.NoValidValueException;
+import fhdw.ipscrum.shared.commands.ticketsGeneral.ListFieldAddValueCommand;
 import fhdw.ipscrum.shared.model.Model;
 import fhdw.ipscrum.shared.model.metamodel.fields.FieldType;
-import fhdw.ipscrum.shared.model.metamodel.fields.FieldTypeVisitor;
 import fhdw.ipscrum.shared.model.metamodel.fields.ListField;
-import fhdw.ipscrum.shared.model.metamodel.fields.SingleField;
+import fhdw.ipscrum.shared.model.metamodel.fields.TextFieldType;
+import fhdw.ipscrum.shared.model.metamodel.ticketsAndTypes.FeatureTicketType;
 import fhdw.ipscrum.shared.model.metamodel.ticketsAndTypes.Ticket;
+import fhdw.ipscrum.shared.model.nonMeta.Feature;
 import fhdw.ipscrum.shared.model.nonMeta.Project;
 import fhdw.ipscrum.shared.model.nonMeta.RelationType;
-import fhdw.ipscrum.shared.model.visitor.TicketVisitor;
 
 /**
  * Tests the ListFieldAddvalueCommand.
@@ -37,8 +35,8 @@ public class ListFieldAddValueCommandTest {
 	 * @throws Exception
 	 *             if one of the used methods fails
 	 */
-	@BeforeClass
-	public void setUpBeforeClass() throws Exception {
+	@Before
+	public void setUpBefore() throws Exception {
 		TestUtils.deleteFolderContent(new File("output"));
 		ServerContext.resetServerContext();
 		this.model = ServerContext.getInstance().getPersistenceManager().getCurrentModel();
@@ -56,70 +54,26 @@ public class ListFieldAddValueCommandTest {
 
 	/**
 	 * Tests the command.
+	 * 
+	 * @throws Exception
+	 *             if one of the used methods fails
 	 */
 	@Test
-	public void test() {
-		@SuppressWarnings("serial")
-		final FieldType type = new FieldType() {
+	public void test() throws Exception {
+		final FeatureTicketType type = new FeatureTicketType(this.model, "ExtendedFeature", "");
+		final FieldType ft = new TextFieldType(this.model, "name", this.model.getTypeManager().getMany());
+		type.addFieldType(ft);
+		type.setActive(this.model.getTypeManager().getOpen(), ft);
+		final ListField<String> field = new ListField<String>(this.getModel(), ft);
+		final Project project = new Project(this.model, "Das tolle Projekt");
+		final Ticket ticket = new Feature(this.model, type, "neuer Bug", "", project.getBacklog());
 
-			@Override
-			protected SingleField<?> createSingleField() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			protected ListField<?> createListField() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public void accept(final FieldTypeVisitor v) {
-				// TODO Auto-generated method stub
-
-			}
-
-		};
-
-		@SuppressWarnings("rawtypes")
-		final ListField field = new ListField(this.getModel(), type);
-		@SuppressWarnings("serial")
-		final Ticket ticket = new Ticket() {
-
-			@Override
-			public Project getProject() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			protected void checkNameValidity(final String name) throws NoValidValueException,
-					DoubleDefinitionException, ConsistencyException {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			protected void checkDescriptionValidity(final String description) throws NoValidValueException {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void accept(final TicketVisitor visitor) {
-				// TODO Auto-generated method stub
-
-			}
-		};
-		// final ListFieldAddValueCommand command =
-		// ListFieldAddValueCommand.createCommand(field, value, ticket);
-		// command.execute(model);
-		Assert.assertNotNull(ticket);
-		Assert.assertNotNull(field);
-		Assert.assertNotNull(type);
-		Assert.fail("Not yet implemented");
+		final String value = "abc";
+		final ListFieldAddValueCommand<String> command =
+				ListFieldAddValueCommand.<String> createCommand(field, value, ticket);
+		command.execute(this.model);
+		Assert.assertEquals(1, field.getValues().size());
+		Assert.assertEquals(value, field.getValues().get(0));
 
 	}
-
 }
